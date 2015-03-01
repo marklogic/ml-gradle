017beca846f3125326a506ce3afde493335a8d60
Rob Rudin <rjrudin@gmail.com>
2015-03-01 14:33:19 -0500

    Added comment

--- src/main/groovy/com/marklogic/gradle/task/cpf/CpfTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/cpf/CpfTask.groovy
@@ -3,6 +3,9 @@ package com.marklogic.gradle.task.cpf
 import com.marklogic.gradle.task.MarkLogicTask
 import com.marklogic.gradle.xcc.XccHelper
 
+/**
+ * Base class for CPF tasks that depend on XCC. These will be replaceable in ML8 with REST API calls.
+ */
 class CpfTask extends MarkLogicTask {
 
     String xccUrl

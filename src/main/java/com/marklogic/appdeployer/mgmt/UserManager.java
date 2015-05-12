de7cea9c145c2fe8c8bd38ccbfd4f6abf27d4cbd
Scott Stafford <scott.stafford@marklogic.com>
2015-05-12 15:10:26 -0400

    UserManager now takes ManageClient as a parameter in constructor

--- src/main/java/com/marklogic/appdeployer/mgmt/UserManager.java
+++ src/main/java/com/marklogic/appdeployer/mgmt/UserManager.java
@@ -1,33 +1,24 @@
 package com.marklogic.appdeployer.mgmt;
 
 import org.jdom2.Namespace;
-import org.springframework.web.client.RestTemplate;
 
+import com.marklogic.appdeployer.mgmt.ManageClient;
 import com.marklogic.appdeployer.util.Fragment;
 import com.marklogic.clientutil.LoggingObject;
 
 public class UserManager extends LoggingObject {
 	
-	private RestTemplate rt;
-  private String baseUrl;
+	private ManageClient client;
 
-  public UserManager(RestTemplate rt, String baseUrl) {
-      this.rt = rt;
-      this.baseUrl = baseUrl;
+  public UserManager(ManageClient client) {
+      this.client = client;
   }
 
   public boolean userExists(String name) {
-      String xml = rt.getForObject(baseUrl + "/manage/v2/users", String.class);
+  		logger.info("Checking for existence of username: " + name);
+      String xml = client.getRestTemplate().getForObject(client.getBaseUrl() + "/manage/v2/users", String.class);
       Fragment f = new Fragment(xml, Namespace.getNamespace("sec", "http://marklogic.com/manage/security"));
       return f.elementExists(String.format("/sec:user-default-list/sec:list-items/sec:list-item[sec:nameref = '%s']", name));
-  }
-
-  public void setRt(RestTemplate rt) {
-      this.rt = rt;
-  }
-
-  public void setBaseUrl(String baseUri) {
-      this.baseUrl = baseUri;
   }
 
 }

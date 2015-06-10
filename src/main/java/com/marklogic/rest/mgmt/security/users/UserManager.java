dacacde9382d8fa5c416b221780d3ab8b82f9065
Scott Stafford <scott.stafford@marklogic.com>
2015-06-09 22:15:45 -0400

    adding some shell code to start

--- src/main/java/com/marklogic/rest/mgmt/security/users/UserManager.java
+++ src/main/java/com/marklogic/rest/mgmt/security/users/UserManager.java
@@ -8,17 +8,21 @@ import com.marklogic.rest.util.Fragment;
 
 public class UserManager extends LoggingObject {
 	
-	private ManageClient client;
+  private ManageClient client;
 
   public UserManager(ManageClient client) {
       this.client = client;
   }
 
   public boolean userExists(String name) {
-  		logger.info("Checking for existence of username: " + name);
-      String xml = client.getRestTemplate().getForObject(client.getBaseUrl() + "/manage/v2/users", String.class);
-      Fragment f = new Fragment(xml, Namespace.getNamespace("sec", "http://marklogic.com/manage/security"));
-      return f.elementExists(String.format("/sec:user-default-list/sec:list-items/sec:list-item[sec:nameref = '%s']", name));
+    logger.info("Checking for existence of username: " + name);
+    String xml = client.getRestTemplate().getForObject(client.getBaseUrl() + "/manage/v2/users", String.class);
+    Fragment f = new Fragment(xml, Namespace.getNamespace("sec", "http://marklogic.com/manage/security"));
+    return f.elementExists(String.format("/sec:user-default-list/sec:list-items/sec:list-item[sec:nameref = '%s']", name));
+  }
+  
+  public void createUser(String user) {
+	  
   }
 
 }
--- src/test/java/com/marklogic/rest/mgmt/security/users/ManageUsersTest.java
+++ src/test/java/com/marklogic/rest/mgmt/security/users/ManageUsersTest.java
@@ -1,21 +1,32 @@
 package com.marklogic.rest.mgmt.security.users;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.marklogic.rest.mgmt.AbstractMgmtTest;
 
 public class ManageUsersTest extends AbstractMgmtTest {
+	
+	UserManager userMgr;
+	
+	@Before
+	public void before() {
+		userMgr = new UserManager(manageClient);
+	}
 
     @Test
     public void doesUserExistTest() {
-        UserManager userMgr = new UserManager(manageClient);
         assertTrue(userMgr.userExists("admin"));
     }
 
     @Test
     public void doesUserNotExistTest() {
-        UserManager userMgr = new UserManager(manageClient);
         assertFalse(userMgr.userExists("admin123"));
+    }
+    
+    @Test
+    public void createUserTest() {
+    	userMgr.createUser("");
     }
 
 }

30d5e4de918a6d30c11c1269633cb7d1e9d2e79a
Rob Rudin <rjrudin@gmail.com>
2015-06-11 07:49:53 -0400

    #22 Firming up this test first

--- src/test/java/com/marklogic/appdeployer/command/modules/LoadModulesTest.java
+++ src/test/java/com/marklogic/appdeployer/command/modules/LoadModulesTest.java
@@ -3,18 +3,30 @@ package com.marklogic.appdeployer.command.modules;
 import org.junit.Test;
 
 import com.marklogic.appdeployer.AbstractAppDeployerTest;
-import com.marklogic.appdeployer.command.modules.LoadModulesCommand;
 import com.marklogic.appdeployer.command.restapis.CreateRestApiServersCommand;
+import com.marklogic.xccutil.template.XccTemplate;
 
 public class LoadModulesTest extends AbstractAppDeployerTest {
 
-    /**
-     * TODO Load from multiple paths
-     */
+    private XccTemplate xccTemplate;
+
     @Test
-    public void loadModules() {
+    public void loadModulesFromMultiplePaths() {
+        appConfig.getModulePaths().add("src/test/resources/sample-app/build/mlRestApi/some-library/ml-modules");
+
         initializeAppDeployer(new CreateRestApiServersCommand(), new LoadModulesCommand());
 
         appDeployer.deploy(appConfig);
+
+        assertModuleExists("sample-lib is loaded from /ext in the default path", "/ext/sample-lib.xqy");
+        assertModuleExists("some-lib.xqy is loaded from the path added at the start of the test", "/ext/some-lib.xqy");
+    }
+
+    private void assertModuleExists(String message, String path) {
+        if (xccTemplate == null) {
+            xccTemplate = newModulesXccTemplate();
+        }
+
+        assertEquals(message, "true", xccTemplate.executeAdhocQuery(format("fn:doc-available('%s')", path)));
     }
 }
---
+++ src/test/resources/sample-app/build/mlRestApi/some-library/ml-modules/ext/some-lib.xqy
@@ -0,0 +1,8 @@
+xquery version "1.0-ml";
+
+module namespace sample = "urn:sampleapp";
+
+declare function foo()
+{
+  ()
+};

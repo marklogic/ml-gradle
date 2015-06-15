bbba23cd798619c07a9d038cac65d99c9fb9e081
Rob Rudin <rjrudin@gmail.com>
2015-06-14 20:02:02 -0400

    Can just configure the command instead

--- src/main/groovy/com/marklogic/gradle/task/client/LoadModulesTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/client/LoadModulesTask.groovy
@@ -7,15 +7,8 @@ import com.marklogic.gradle.task.MarkLogicTask
 
 class LoadModulesTask extends MarkLogicTask {
 
-    String assetRolesAndCapabilities
-
     @TaskAction
     void loadModules() {
-        println "Loading modules from paths: " + getAppConfig().getModulePaths()
-        LoadModulesCommand c = new LoadModulesCommand()
-        if (assetRolesAndCapabilities != null) {
-            c.setAssetRolesAndCapabilities(assetRolesAndCapabilities)
-        }
-        c.execute(getCommandContext())
+        new LoadModulesCommand().execute(getCommandContext())
     }
 }

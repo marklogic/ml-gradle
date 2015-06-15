b7b03089418d7c70a97c66fc49dced22427995e7
Rob Rudin <rjrudin@gmail.com>
2015-06-15 08:24:45 -0400

    Restoring CPF code, new stuff won't be ready for a while

---
+++ src/main/groovy/com/marklogic/gradle/task/cpf/InsertAlertingPipelineTask.groovy
@@ -0,0 +1,8 @@
+package com.marklogic.gradle.task.cpf
+
+class InsertAlertingPipelineTask extends InsertSystemPipelineTask {
+
+    public InsertAlertingPipelineTask() {
+        setFilename("Installer/alert/alerting-pipeline.xml")
+    }
+}
---
+++ src/main/groovy/com/marklogic/gradle/task/cpf/InsertSchPipelineTask.groovy
@@ -0,0 +1,8 @@
+package com.marklogic.gradle.task.cpf
+
+class InsertSchPipelineTask extends InsertSystemPipelineTask {
+
+    public InsertSchPipelineTask() {
+        setFilename("Installer/cpf/status-pipeline.xml")
+    }
+}

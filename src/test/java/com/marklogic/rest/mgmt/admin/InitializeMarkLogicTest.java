dc65297c1a849b820be5d0cf6468e44d9bb086ec
Rob Rudin <rjrudin@gmail.com>
2015-06-22 09:35:18 -0400

    Added init and installAdmin

---
+++ src/test/java/com/marklogic/rest/mgmt/admin/InitializeMarkLogicTest.java
@@ -0,0 +1,18 @@
+package com.marklogic.rest.mgmt.admin;
+
+import org.junit.Test;
+
+import com.marklogic.rest.mgmt.AbstractMgmtTest;
+
+public class InitializeMarkLogicTest extends AbstractMgmtTest {
+
+    /**
+     * The only way to really test this is to run it against a freshly installed MarkLogic, but in a test suite, we
+     * always assume that we have a MarkLogic instance that has been initialized already. So this is just a smoke test
+     * to ensure no errors are thrown from bad JSON.
+     */
+    @Test
+    public void initAgainstAnAlreadyInitializedMarkLogic() {
+        adminManager.init();
+    }
+}

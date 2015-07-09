d870212836ff957074e81ddef5e4d64570c06ecc
Rob Rudin <rjrudin@gmail.com>
2015-07-09 14:45:13 -0400

    #32 Gotta delete the timestamps file

--- src/test/java/com/marklogic/appdeployer/command/viewschemas/ManageViewSchemasTest.java
+++ src/test/java/com/marklogic/appdeployer/command/viewschemas/ManageViewSchemasTest.java
@@ -6,7 +6,6 @@ import org.springframework.web.client.RestTemplate;
 
 import com.marklogic.appdeployer.command.AbstractManageResourceTest;
 import com.marklogic.appdeployer.command.Command;
-import com.marklogic.appdeployer.command.appservers.CreateOtherServersCommand;
 import com.marklogic.appdeployer.command.databases.CreateSchemasDatabaseCommand;
 import com.marklogic.appdeployer.command.databases.CreateTriggersDatabaseCommand;
 import com.marklogic.appdeployer.command.databases.UpdateContentDatabasesCommand;
@@ -21,11 +20,12 @@ public class ManageViewSchemasTest extends AbstractManageResourceTest {
 
     @Override
     protected void initializeAndDeploy() {
-        initializeAppDeployer(new CreateRestApiServersCommand(), new CreateSchemasDatabaseCommand(),
-                new CreateOtherServersCommand(), new CreateTriggersDatabaseCommand(),
-                new UpdateContentDatabasesCommand(), newCommand(), new LoadModulesCommand());
+        deleteModuleTimestampsFile();
 
-        appConfig.getCustomTokens().put("%%ODBC_PORT%%", "8542");
+        initializeAppDeployer(new CreateRestApiServersCommand(), new CreateSchemasDatabaseCommand(),
+                new CreateTriggersDatabaseCommand(), new UpdateContentDatabasesCommand(), newCommand(),
+                new LoadModulesCommand());
+
         appDeployer.deploy(appConfig);
     }
 

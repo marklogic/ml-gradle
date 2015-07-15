5544f1f9fe90fee17923933b568861594156c60b
Rob Rudin <rjrudin@gmail.com>
2015-07-15 08:24:20 -0400

    New tasks for clearing the triggers/schemas databases

---
+++ src/main/groovy/com/marklogic/gradle/task/databases/ClearTriggersDatabaseTask.groovy
@@ -0,0 +1,17 @@
+package com.marklogic.gradle.task.databases
+
+import org.gradle.api.tasks.TaskAction
+
+import com.marklogic.gradle.task.MarkLogicTask
+import com.marklogic.rest.mgmt.databases.DatabaseManager
+
+class ClearTriggersDatabaseTask extends MarkLogicTask {
+
+    @TaskAction
+    void clearModules() {
+        println "Clearing all documents in triggers database"
+        DatabaseManager mgr = new DatabaseManager(getManageClient())
+        mgr.clearDatabase(getAppConfig().getTriggersDatabaseName())
+        println "Finished clearing all documents in triggers database"
+    }
+}

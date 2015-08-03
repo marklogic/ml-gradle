5d2be6bbc0aac3f8967430ee35b9379236e12184
Rob Rudin <rjrudin@gmail.com>
2015-08-03 10:01:58 -0400

    #38 Combining the two tests into one

--- src/test/java/com/marklogic/appdeployer/command/groups/ManageGroupsTest.java
+++ src/test/java/com/marklogic/appdeployer/command/groups/ManageGroupsTest.java
@@ -1,54 +1,25 @@
 package com.marklogic.appdeployer.command.groups;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import com.marklogic.appdeployer.command.AbstractManageResourceTest;
 import com.marklogic.appdeployer.command.Command;
 import com.marklogic.rest.mgmt.ResourceManager;
+import com.marklogic.rest.mgmt.groups.GroupManager;
 
-public class ManageGroupsTest extends AbstractManageResourceTest{
+public class ManageGroupsTest extends AbstractManageResourceTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @Override
+    protected ResourceManager newResourceManager() {
+        return new GroupManager(manageClient);
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @Override
+    protected Command newCommand() {
+        return new CreateGroupsCommand();
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-	@Override
-	protected ResourceManager newResourceManager() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected Command newCommand() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected String[] getResourceNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    protected String[] getResourceNames() {
+        return new String[] { "sample-app-group" };
+    }
 
 }

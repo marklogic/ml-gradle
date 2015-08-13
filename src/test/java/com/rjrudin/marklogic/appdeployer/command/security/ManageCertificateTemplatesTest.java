7f5bec988af71cb5133dd093ab0077adee2cb872
Rob Rudin <rjrudin@gmail.com>
2015-08-12 21:33:41 -0400

    #47 Removed OBE test

--- src/test/java/com/rjrudin/marklogic/appdeployer/command/security/ManageCertificateTemplatesTest.java
+++ src/test/java/com/rjrudin/marklogic/appdeployer/command/security/ManageCertificateTemplatesTest.java
@@ -9,12 +9,6 @@ import com.rjrudin.marklogic.mgmt.security.CertificateTemplateManager;
 
 public class ManageCertificateTemplatesTest extends AbstractManageResourceTest {
 
-    // @Test
-    public void rob() {
-        manageClient.putJson("/manage/v2/servers/sample-project/properties?group-id=Default",
-                "{\"ssl-certificate-template\":\"10426856940027527644\"}");
-    }
-
     @Override
     protected ResourceManager newResourceManager() {
         return new CertificateTemplateManager(manageClient);

0f5015a5fe99ade28cc37eaa78113b0b909dd5f7
Rob Rudin <rjrudin@gmail.com>
2015-08-26 08:06:00 -0400

    Don't try to delete the Default group

--- src/main/java/com/rjrudin/marklogic/mgmt/groups/GroupManager.java
+++ src/main/java/com/rjrudin/marklogic/mgmt/groups/GroupManager.java
@@ -5,12 +5,21 @@ import com.rjrudin.marklogic.mgmt.ManageClient;
 
 public class GroupManager extends AbstractResourceManager {
 
-	public GroupManager(ManageClient manageClient) {
-		super(manageClient);
-	}
+    public GroupManager(ManageClient manageClient) {
+        super(manageClient);
+    }
 
     @Override
     protected boolean useAdminUser() {
         return true;
+    }
+
+    @Override
+    public boolean delete(String payload) {
+        String resourceId = getResourceId(payload);
+        if (resourceId != null && resourceId.toUpperCase().equals("DEFAULT")) {
+            return false;
+        }
+        return super.delete(payload);
     }
 }

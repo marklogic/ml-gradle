1edf432225790ca089771361a35850a987ecbc42
Rob Rudin <rjrudin@gmail.com>
2015-08-30 20:22:38 -0400

    #29 New config dir that's setup for flexrep testing

---
+++ src/test/java/com/rjrudin/marklogic/appdeployer/command/flexrep/DeployFlexrepTest.java
@@ -0,0 +1,26 @@
+package com.rjrudin.marklogic.appdeployer.command.flexrep;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.rjrudin.marklogic.appdeployer.AbstractAppDeployerTest;
+import com.rjrudin.marklogic.appdeployer.command.cpf.DeployCpfConfigsCommand;
+import com.rjrudin.marklogic.appdeployer.command.cpf.DeployDomainsCommand;
+import com.rjrudin.marklogic.appdeployer.command.cpf.DeployPipelinesCommand;
+import com.rjrudin.marklogic.appdeployer.command.databases.DeployContentDatabasesCommand;
+import com.rjrudin.marklogic.appdeployer.command.databases.DeployTriggersDatabaseCommand;
+
+public class DeployFlexrepTest extends AbstractAppDeployerTest {
+
+    @Test
+    public void test() {
+        appConfig.getConfigDir().setBaseDir(new File("src/test/resources/sample-app/flexrep-config"));
+
+        initializeAppDeployer(new DeployContentDatabasesCommand(), new DeployTriggersDatabaseCommand(),
+                new DeployCpfConfigsCommand(), new DeployDomainsCommand(), new DeployPipelinesCommand());
+
+        appDeployer.deploy(appConfig);
+
+    }
+}

172ababf819e8993889165ac5cd35887496801d4
Rob Rudin <rjrudin@gmail.com>
2015-09-22 07:38:43 -0400

    2.0b9 ml-gradle-all

--- ml-gradle-all/src/main/java/com/rjrudin/marklogic/mlgradleall/MlGradlePackager.java
+++ ml-gradle-all/src/main/java/com/rjrudin/marklogic/mlgradleall/MlGradlePackager.java
@@ -21,7 +21,7 @@ public class MlGradlePackager {
             destDir.delete();
         }
         destDir.mkdirs();
-        processDependency("ml-gradle", "2.0b8");
+        processDependency("ml-gradle", "2.0b9");
         processDependency("ml-junit", "2.3");
     }
 

72d384040f04b35e98f8f400d096ff8154e6f7ea
Rob Rudin <rjrudin@gmail.com>
2015-11-30 22:33:07 -0500

    Alert example project

---
+++ examples/alert-project/src/test/java/test/TestConfig.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.rjrudin.marklogic.junit.spring.BasicTestConfig;
+
+@Configuration
+@PropertySource({ "file:gradle.properties" })
+public class TestConfig extends BasicTestConfig {
+
+    /**
+     * Overriding this since this project currently doesn't have a test database, just the single content database.
+     */
+    @Override
+    protected String buildContentDatabaseName(String mlAppName) {
+        return mlAppName + "-content";
+    }
+
+}

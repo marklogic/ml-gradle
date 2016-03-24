7cadcac729847c6175826eb6f219bb8029b7ee32
Rob Rudin <rjrudin@gmail.com>
2016-03-24 19:57:12 -0400

    #88 New command for deploying all flexrep resources from flexrep/master or flexrep/replica

---
+++ src/main/java/com/marklogic/appdeployer/command/flexrep/DeployFlexrepCommand.java
@@ -0,0 +1,96 @@
+package com.marklogic.appdeployer.command.flexrep;
+
+import com.marklogic.appdeployer.AppConfig;
+import com.marklogic.appdeployer.command.AbstractCommand;
+import com.marklogic.appdeployer.command.CommandContext;
+import com.marklogic.appdeployer.command.SortOrderConstants;
+import com.marklogic.appdeployer.command.UndoableCommand;
+import com.marklogic.appdeployer.command.appservers.DeployOtherServersCommand;
+import com.marklogic.appdeployer.command.cpf.DeployCpfConfigsCommand;
+import com.marklogic.appdeployer.command.cpf.DeployDomainsCommand;
+import com.marklogic.appdeployer.command.cpf.DeployPipelinesCommand;
+import com.marklogic.appdeployer.impl.SimpleAppDeployer;
+
+import java.io.File;
+
+/**
+ * This command is for deploying all resources associated with a flexrep config. It combines CPF, flexrep configs
+ * and targets, and optionally an HTTP server by reusing other commands. The intent is to support a configuration
+ * for both a master and a replica in the same project. Most of the time you won't need this, in which case you
+ * can just use DeployConfigsCommand and DeployTargetsCommand.
+ */
+public class DeployFlexrepCommand extends AbstractCommand implements UndoableCommand {
+
+    private String path;
+
+    public DeployFlexrepCommand() {
+        this("master");
+    }
+
+    public DeployFlexrepCommand(String path) {
+        this.path = path;
+        setExecuteSortOrder(SortOrderConstants.DEPLOY_OTHER_SERVERS);
+    }
+
+    @Override
+    public Integer getUndoSortOrder() {
+        return SortOrderConstants.DELETE_OTHER_SERVERS;
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        AppConfig appConfig = context.getAppConfig();
+        File flexrepBaseDir = getFlexrepBaseDir(appConfig);
+        if (flexrepBaseDir != null) {
+            SimpleAppDeployer d = new SimpleAppDeployer(context.getManageClient(), context.getAdminManager(),
+                    new DeployCpfConfigsCommand(), new DeployDomainsCommand(), new DeployPipelinesCommand(), new DeployConfigsCommand(), new DeployTargetsCommand(), new DeployOtherServersCommand());
+            File currentBaseDir = appConfig.getConfigDir().getBaseDir();
+            appConfig.getConfigDir().setBaseDir(flexrepBaseDir);
+            try {
+                d.deploy(appConfig);
+            } finally {
+                appConfig.getConfigDir().setBaseDir(currentBaseDir);
+            }
+        }
+    }
+
+
+    @Override
+    public void undo(CommandContext context) {
+        AppConfig appConfig = context.getAppConfig();
+        File flexrepBaseDir = getFlexrepBaseDir(appConfig);
+        if (flexrepBaseDir != null) {
+            SimpleAppDeployer d = new SimpleAppDeployer(context.getManageClient(), context.getAdminManager(), new DeployOtherServersCommand());
+            File currentBaseDir = appConfig.getConfigDir().getBaseDir();
+            appConfig.getConfigDir().setBaseDir(flexrepBaseDir);
+            try {
+                d.undeploy(appConfig);
+            } finally {
+                appConfig.getConfigDir().setBaseDir(currentBaseDir);
+            }
+        }
+    }
+
+    protected File getFlexrepBaseDir(AppConfig appConfig) {
+        if (path == null) {
+            return null;
+        }
+
+        File flexrepDir = appConfig.getConfigDir().getFlexrepDir();
+        if (flexrepDir == null || !flexrepDir.exists()) {
+            return null;
+        }
+
+        File flexrepBaseDir = new File(flexrepDir, path);
+        if (flexrepBaseDir == null || !flexrepBaseDir.exists()) {
+            return null;
+        }
+
+        return flexrepBaseDir;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+}

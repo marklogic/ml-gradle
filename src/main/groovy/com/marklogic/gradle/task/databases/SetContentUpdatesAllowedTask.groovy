9d896d2224fe8ae8abc40bf38ec48bac8882de17
Rob Rudin <rjrudin@gmail.com>
2016-04-03 09:58:45 -0400

    #75 New task for setting updates-allowed on content forests

---
+++ src/main/groovy/com/marklogic/gradle/task/databases/SetContentUpdatesAllowedTask.groovy
@@ -0,0 +1,22 @@
+package com.marklogic.gradle.task.databases
+
+import org.gradle.api.tasks.TaskAction
+
+import com.marklogic.gradle.task.MarkLogicTask
+import com.marklogic.mgmt.databases.DatabaseManager
+
+class SetContentUpdatesAllowedTask extends MarkLogicTask {
+
+    @TaskAction
+    void clearModules() {
+        if (project.hasProperty("mode")) {
+            def mode = project.property("mode")
+            println "Setting updates-allowed on each content forest to " + mode
+            DatabaseManager mgr = new DatabaseManager(getManageClient())
+            mgr.setUpdatesAllowedOnPrimaryForests(getAppConfig().getContentDatabaseName(), mode)
+        } else {
+            println "To set updates-allowed on each forest in the content database, include the mode parameter e.g. -Pmode=flash-backup"
+            return
+        }
+    }
+}

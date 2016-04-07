5463f66defceccd06afd153816ad4b6ddc605a1d
Rob Rudin <rjrudin@gmail.com>
2016-04-07 08:21:30 -0400

    #22 Fixing bug in getExtension

--- src/main/java/com/marklogic/client/schemasloader/impl/DefaultSchemasLoader.java
+++ src/main/java/com/marklogic/client/schemasloader/impl/DefaultSchemasLoader.java
@@ -17,41 +17,39 @@ import com.marklogic.client.schemasloader.SchemasFinder;
 import com.marklogic.client.schemasloader.SchemasLoader;
 
 public class DefaultSchemasLoader extends LoggingObject implements SchemasLoader {
-	
-	@Override
-	public Set<File> loadSchemas(File baseDir, SchemasFinder schemasDataFinder, DatabaseClient client) {
-		XMLDocumentManager xmlDocMgr = client.newXMLDocumentManager();
-		TextDocumentManager textDocMgr = client.newTextDocumentManager();
-		JSONDocumentManager jsonDocMgr = client.newJSONDocumentManager();
-		
-		List<File> schemasData = schemasDataFinder.findSchemas(baseDir);
+
+    @Override
+    public Set<File> loadSchemas(File baseDir, SchemasFinder schemasDataFinder, DatabaseClient client) {
+        XMLDocumentManager xmlDocMgr = client.newXMLDocumentManager();
+        TextDocumentManager textDocMgr = client.newTextDocumentManager();
+        JSONDocumentManager jsonDocMgr = client.newJSONDocumentManager();
+
+        List<File> schemasData = schemasDataFinder.findSchemas(baseDir);
 
         Set<File> loadedSchemas = new HashSet<>();
-        DocumentMetadataHandle tdeCollection = new DocumentMetadataHandle().withCollections("http://marklogic.com/xdmp/tde");
+        DocumentMetadataHandle tdeCollection = new DocumentMetadataHandle()
+                .withCollections("http://marklogic.com/xdmp/tde");
         for (File f : schemasData) {
-        	String extension = getExtensionNameFromFile(f);
-        	FileHandle handle = new FileHandle(f);
-        	if (extension.equals("tdej")) {
-        		jsonDocMgr.write(f.getName(), tdeCollection, handle.withFormat(Format.JSON));
-        	}
-        	else if (extension.equals("tdex")) {
-        		xmlDocMgr.write(f.getName(), tdeCollection, handle.withFormat(Format.XML));
-        	}
-        	else if (extension.equals("xsd")) {
-        		xmlDocMgr.write(f.getName(), handle.withFormat(Format.XML));
-        	} else {
-        		textDocMgr.write(f.getName(), handle.withFormat(Format.TEXT));
-        	}
-        	loadedSchemas.add(f);
+            String extension = getExtensionNameFromFile(f);
+            FileHandle handle = new FileHandle(f);
+            if (extension.equals("tdej")) {
+                jsonDocMgr.write(f.getName(), tdeCollection, handle.withFormat(Format.JSON));
+            } else if (extension.equals("tdex")) {
+                xmlDocMgr.write(f.getName(), tdeCollection, handle.withFormat(Format.XML));
+            } else if (extension.equals("xsd")) {
+                xmlDocMgr.write(f.getName(), handle.withFormat(Format.XML));
+            } else {
+                textDocMgr.write(f.getName(), handle.withFormat(Format.TEXT));
+            }
+            loadedSchemas.add(f);
         }
 
         return loadedSchemas;
-	}
-	 protected String getExtensionNameFromFile(File file) {
-	        String name = file.getName();
-	        int pos = name.lastIndexOf('.');
-	        if (pos < 0)
-	            return name;
-	        return name.substring(0, pos);
-	    }
+    }
+
+    protected String getExtensionNameFromFile(File file) {
+        String name = file.getName();
+        int pos = name.lastIndexOf('.');
+        return pos < 0 ? name : name.substring(pos + 1);
+    }
 }

28bb8f2c5d7075d3190254397a3df4df9a991cb5
Mads Hansen <mads.hansen@marklogic.com>
2016-04-11 23:01:30 -0400

    update to CORB 2.3.0
    Remove hard-coded list of CORB Options and use reflection to get them
    from the CORB Options class.
    
    Remove reference to rjrudin/maven repo, now that CORB is available via
    jcenter().

--- src/main/groovy/com/marklogic/gradle/task/CorbTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/CorbTask.groovy
@@ -8,47 +8,27 @@ import com.marklogic.appdeployer.AppConfig
 class CorbTask extends JavaExec {
 
   CorbTask() {
-    //Augment with member variables and a mapping of CoRB2 options
-    /*
-    * TODO: replace hard-coded CSV of options with a reference to the CoRB Options class,
-    *       once the next version of CoRB2 is released:
-    *
-      CorbTask.metaClass.corbOptions = com.marklogic.developer.corb.Options.class.declaredFields.findAll { !it.synthetic }.collectEntries {
-    */
-    this.metaClass.corbOptions = "BATCH-SIZE,BATCH-URI-DELIM,COLLECTION-NAME,DECRYPTER,\
-ERROR-FILE-NAME,EXIT-CODE-NO-URIS,EXPORT_FILE_AS_ZIP,\
-EXPORT-FILE-BOTTOM-CONTENT,EXPORT-FILE-DIR,EXPORT-FILE-HEADER-LINE-COUNT,\
-EXPORT-FILE-NAME,EXPORT-FILE-PART-EXT,\
-EXPORT-FILE-SORT,EXPORT-FILE-SORT-COMPARATOR,\
-EXPORT-FILE-TOP-CONTENT,EXPORT-FILE-URI-TO-PATH,\
-FAIL-ON-ERROR,INSTALL,INIT-MODULE,INIT-TASK,JASYPT-PROPERTIES-FILE,\
-MAX_OPTS_FROM_MODULE,MODULES-DATABASE,MODULE-ROOT,OPTIONS-FILE,\
-POST-BATCH-MODULE,POST-BATCH-TASK,POST-BATCH-XQUERY-MODULE,\
-PRE-BATCH-MODULE,PRE-BATCH-TASK,PRE-BATCH-XQUERY-MODULE,\
-PRIVATE-KEY-ALGORITHM,PRIVATE-KEY-FILE,\
-PROCESS-MODULE,PROCESS-TASK,\
-QUERY-RETRY-LIMIT,QUERY-RETRY-INTERVAL,\
-SSL-CIPHER-SUITES,SSL-CONFIG-CLASS,SSL-ENABLED-PROTOCOLS,SSL-KEY-PASSWORD,\
-SSL-KEYSTORE,SSL-KEYSTORE-PASSWORD,SSL-KEYSTORE-TYPE,SSL-PROPERTIES-FILE,\
-THREAD-COUNT,URIS_BATCH_REF,URIS-FILE,URIS-LOADER,URIS-MODULE,URIS-REPLACE-PATTERN,\
-XCC_CONNECTION_RETRY_LIMIT,XCC-CONNECTION-RETRY-INTERVAL,XCC-CONNECTION-URI,\
-XCC-DBNAME,XCC-HOSTNAME,XCC-PASSWORD,XCC-PORT,XCC-USERNAME,\
-XQUERY_MODULE"
-      .tokenize(',').collectEntries {
-        String camel = it.toLowerCase().split('_|-').collect { it.capitalize() }.join('')
-        // create Map entry gradle property and original values, for easy lookup/translation
-        String lowerCamel = new StringBuffer(camel.length())
-                  .append(Character.toLowerCase(camel.charAt(0)))
-                  .append(camel.substring(1))
-                  .toString();
+      System.out.println("initializing CorbTask " )
+    //Augment with member variables and a mapping of CoRB2 Options
+    CorbTask.metaClass.corbOptions = Class.forName("com.marklogic.developer.corb.Options", true, Thread.currentThread().contextClassLoader)
+        .declaredFields
+        .findAll { !it.synthetic }
+        .collectEntries {
+            String option = it.name
+            String camelOption = option.toLowerCase().split('_|-').collect { it.capitalize() }.join('')
+            // create Map entry gradle property and original values, for easy lookup/translation
+            String lowerCamelOption = new StringBuffer(camelOption.length())
+                                        .append(Character.toLowerCase(camelOption.charAt(0)))
+                                        .append(camelOption.substring(1))
+                                        .toString();
 
-        //add the lowerCamelCased CoRB2 option as a member variable
-        this.metaClass[lowerCamel] = null
+            //add the lowerCamelCased CoRB2 option as a member variable
+            this.metaClass[lowerCamelOption] = null
 
-        // Create a 'corb' prefixed camelCased entry (i.e. URIS-FILE => corbUrisFile )
-        // mapped to the original CoRB2 option for lookup/conversion
-        [(CORB_PROPERTY_PREFIX + camel): it]
-    }
+            // Create a 'corb' prefixed CamelCased entry (i.e. URIS-FILE => corbUrisFile )
+            // mapped to the original CoRB2 option for lookup/conversion
+            [(CORB_PROPERTY_PREFIX + camelOption): option]
+        }
   }
   // prefix for corb project properties, to ensure no conflicts with other project properties
   private static final String CORB_PROPERTY_PREFIX = "corb"
@@ -70,6 +50,7 @@ XQUERY_MODULE"
   @TaskAction
   @Override
   public void exec() {
+    //By convention, if there is a corb configuration, use it to set the classpath  
     if (getProject().configurations.findByName('corb')) {
       setClasspath(getProject().configurations.corb)
     }
@@ -171,4 +152,5 @@ XQUERY_MODULE"
       [(corbOptions[it]): project[it]]
     }
   }
+  
 }

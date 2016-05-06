45063c2b323126b79a0342d3e814bc6e3b919b3b
Rob Rudin <rjrudin@gmail.com>
2016-05-06 09:28:44 -0400

    #80 Restarting cluster in a way that works for 8.0-4 and 8.0-5

--- src/main/groovy/com/marklogic/gradle/task/cluster/RestartClusterTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/cluster/RestartClusterTask.groovy
@@ -1,21 +1,20 @@
 package com.marklogic.gradle.task.cluster
 
-import org.gradle.api.tasks.TaskAction
-
 import com.marklogic.gradle.task.MarkLogicTask
 import com.marklogic.mgmt.ManageClient
 import com.marklogic.mgmt.admin.ActionRequiringRestart
+import org.gradle.api.tasks.TaskAction
 
 class RestartClusterTask extends MarkLogicTask {
 
-    @TaskAction
-    void restartCluster() {
-        final ManageClient client = getManageClient();
-        getAdminManager().invokeActionRequiringRestart(new ActionRequiringRestart() {
-                    public boolean execute() {
-                        client.postJson("/manage/v2", "{\"operation\":\"restart-local-cluster\"}");
-                        return true;
-                    }
-                });
-    }
+	@TaskAction
+	void restartCluster() {
+		final ManageClient client = getManageClient();
+		getAdminManager().invokeActionRequiringRestart(new ActionRequiringRestart() {
+			public boolean execute() {
+				client.postForm("/manage/v2", "state", "restart")
+				return true;
+			}
+		});
+	}
 }

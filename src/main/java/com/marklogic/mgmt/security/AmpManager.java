b92a191dce6e208e1e93155d74a3965bbeca2f98
Rob Rudin <rjrudin@gmail.com>
2016-09-30 09:04:21 -0400

    #60 Can now deploy/undeploy amps correctly - at least on 8.0-5.8

--- src/main/java/com/marklogic/mgmt/security/AmpManager.java
+++ src/main/java/com/marklogic/mgmt/security/AmpManager.java
@@ -1,120 +1,185 @@
 package com.marklogic.mgmt.security;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.marklogic.mgmt.AbstractResourceManager;
 import com.marklogic.mgmt.ManageClient;
+import com.marklogic.mgmt.SaveReceipt;
 import com.marklogic.rest.util.Fragment;
+import com.marklogic.rest.util.ResourcesFragment;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AmpManager extends AbstractResourceManager {
 
-    private String namespace;
-    private String documentUri;
-    private String modulesDatabase;
+	private String namespace;
+	private String documentUri;
+	private String modulesDatabase;
 
-    public AmpManager(ManageClient client) {
-        super(client);
-        /*
-         * Turning this off as part of version 2.0b10 - having issues with ML being able to create an amp but then
-         * getting a 404 when it tries to update the amp. Seems to be due to using a modules database - viewing the amp
-         * via the Mgmt API shows the modules database as being "filesystem", but the Admin app correctly shows the
-         * modules database.
-         */
-        setUpdateAllowed(false);
-    }
+	public AmpManager(ManageClient client) {
+		super(client);
+	}
 
-    public String getResourcePath(String resourceNameOrId) {
-        return super.getResourcePath(resourceNameOrId, "namespace", namespace, "document-uri", documentUri,
-                "modules-database", modulesDatabase);
-    }
+	public String getResourcePath(String resourceNameOrId) {
+		return super.getResourcePath(resourceNameOrId, "namespace", namespace, "document-uri", documentUri,
+			"modules-database", modulesDatabase);
+	}
 
-    public String getPropertiesPath(String resourceNameOrId) {
-        return super.getPropertiesPath(resourceNameOrId, "namespace", namespace, "document-uri", documentUri,
-                "modules-database", modulesDatabase);
-    }
+	public String getPropertiesPath(String resourceNameOrId) {
+		return super.getPropertiesPath(resourceNameOrId, "namespace", namespace, "document-uri", documentUri,
+			"modules-database", modulesDatabase);
+	}
 
-    @Override
-    protected boolean useAdminUser() {
-        return true;
-    }
+	@Override
+	protected boolean useAdminUser() {
+		return true;
+	}
 
-    @Override
-    protected String getIdFieldName() {
-        return "local-name";
-    }
+	@Override
+	protected String getIdFieldName() {
+		return "local-name";
+	}
 
-    @Override
-    protected String[] getUpdateResourceParams(String payload) {
-        List<String> params = new ArrayList<String>();
-        params.add("document-uri");
-        if (payloadParser.isJsonPayload(payload)) {
-            JsonNode node = payloadParser.parseJson(payload);
-            params.add(node.get("document-uri").asText());
-            if (node.has("namespace")) {
-                params.add("namespace");
-                params.add(node.get("namespace").asText());
-            }
-            if (node.has("modules-database")) {
-                params.add("modules-database");
-                params.add(node.get("modules-database").asText());
-            }
-        } else {
-            Fragment f = new Fragment(payload);
-            params.add(f.getElementValue("/node()/*[local-name(.) = 'document-uri']"));
+	/**
+	 * We have to override how this works in the parent class because the parent class assumes that existence
+	 * can be based solely on the resource ID. But for an amp, we need to use the resource ID (local-name),
+	 * document-uri, namespace, and modules-database.
+	 */
+	@Override
+	public SaveReceipt save(String payload) {
+		String resourceId = getResourceId(payload);
+		String label = getResourceName();
+		String path = null;
+		ResponseEntity<String> response = null;
+		if (ampExists(payload)) {
+			return updateResource(payload, resourceId);
+		} else {
+			logger.info(format("Creating %s: %s", label, resourceId));
+			path = getCreateResourcePath(payload);
+			response = postPayload(getManageClient(), path, payload);
+			logger.info(format("Created %s: %s", label, resourceId));
+		}
+		return new SaveReceipt(resourceId, payload, path, response);
+	}
 
-            String val = f.getElementValue("/node()/*[local-name(.) = 'namespace']");
-            if (val != null) {
-                params.add("namespace");
-                params.add(val);
-            }
+	/**
+	 * Uses local-name, document-uri, modules-database, and namespace to determine if the amp exists. Since we
+	 * do this comparison against the XML returned by /manage/v2/amps, if no modules-database is set, we have
+	 * to substitute in the value "filesystem", as that's what is returned by that endpoint.
+	 *
+	 * @param payload
+	 * @return
+	 */
+	public boolean ampExists(String payload) {
+		String resourceId = getResourceId(payload);
+		AmpParams params = getAmpParams(payload);
+		ResourcesFragment resources = getAsXml();
 
-            val = f.getElementValue("/node()/*[local-name(.) = 'modules-database']");
-            if (val != null) {
-                params.add("modules-database");
-                params.add(val);
-            }
-        }
-        return params.toArray(new String[] {});
-    }
+		String xpath = "/node()/*[local-name(.) = 'list-items']/node()[" +
+			"(*[local-name(.) = 'nameref'] = '%s' or *[local-name(.) = 'idref'] = '%s')" +
+			" and *[local-name(.) = 'document-uri'] = '%s'";
+		xpath = format(xpath, resourceId, resourceId, params.documentUri);
+		if (params.namespace != null) {
+			xpath += format(" and *[local-name(.) = 'namespace'] = '%s'", params.namespace);
+		}
+		if (params.modulesDatabase != null) {
+			xpath += format(" and *[local-name(.) = 'modules-database'] = '%s'", params.modulesDatabase);
+		} else {
+			xpath += format(" and *[local-name(.) = 'modules-database'] = 'filesystem'");
+		}
+		xpath += "]";
+		return resources.elementExists(xpath);
+	}
 
-    /**
-     * In ML 8.0-5.1 deleting an amp fails if the database is specified on the
-     * delete url
-     */
-    @Override
-    protected String[] getDeleteResourceParams(String payload) {
-        List<String> params = new ArrayList<String>();
-        params.add("document-uri");
-        if (payloadParser.isJsonPayload(payload)) {
-            JsonNode node = payloadParser.parseJson(payload);
-            params.add(node.get("document-uri").asText());
-            if (node.has("namespace")) {
-                params.add("namespace");
-                params.add(node.get("namespace").asText());
-            }
-        } else {
-            Fragment f = new Fragment(payload);
-            params.add(f.getElementValue("/node()/*[local-name(.) = 'document-uri']"));
+	@Override
+	protected String[] getUpdateResourceParams(String payload) {
+		List<String> params = new ArrayList<String>();
+		AmpParams ampParams = getAmpParams(payload);
+		params.add("document-uri");
+		params.add(ampParams.documentUri);
+		if (ampParams.namespace != null) {
+			params.add("namespace");
+			params.add(ampParams.namespace);
+		}
+		if (ampParams.modulesDatabase != null) {
+			params.add("modules-database");
+			params.add(ampParams.modulesDatabase);
+		}
+		return params.toArray(new String[]{});
+	}
 
-            String val = f.getElementValue("/node()/*[local-name(.) = 'namespace']");
-            if (val != null) {
-                params.add("namespace");
-                params.add(val);
-            }
-        }
-        return params.toArray(new String[] {});    }
+	/**
+	 * Note that the parent class's delete method does an existence check based on just the resource ID - in an
+	 * amp's case, the local name. That will still work, because when the parent class builds the properties path
+	 * for the amp resource, it will call this method and include the other parameters needed to uniquely refer
+	 * to the amp. Worst case - a call is made to delete an amp that no longer exists, but another amp exists with
+	 * the same local name. The parent class will think the amp exists, but when it tries the delete call, it
+	 * won't find the amp with all the params, and the call will succeed without deleting anything.
+	 */
+	@Override
+	protected String[] getDeleteResourceParams(String payload) {
+		List<String> params = new ArrayList<String>();
+		AmpParams ampParams = getAmpParams(payload);
+		params.add("document-uri");
+		params.add(ampParams.documentUri);
+		if (ampParams.namespace != null) {
+			params.add("namespace");
+			params.add(ampParams.namespace);
+		}
+		if (ampParams.modulesDatabase != null) {
+			params.add("modules-database");
+			params.add(ampParams.modulesDatabase);
+		}
+		return params.toArray(new String[]{});
+	}
 
-    public void setNamespace(String namespace) {
-        this.namespace = namespace;
-    }
+	/**
+	 * @param payload
+	 * @return an AmpParams object containing the values of the 3 amp properties - besides local name - that are
+	 * needed to uniquely refer to an amp.
+	 */
+	public AmpParams getAmpParams(String payload) {
+		AmpParams params = new AmpParams();
+		if (payloadParser.isJsonPayload(payload)) {
+			JsonNode node = payloadParser.parseJson(payload);
+			params.documentUri = node.get("document-uri").asText();
+			if (node.has("namespace")) {
+				params.namespace = node.get("namespace").asText();
+			}
+			if (node.has("modules-database")) {
+				params.modulesDatabase = node.get("modules-database").asText();
+			}
+		} else {
+			Fragment f = new Fragment(payload);
+			params.documentUri = f.getElementValue("/node()/*[local-name(.) = 'document-uri']");
+			String val = f.getElementValue("/node()/*[local-name(.) = 'namespace']");
+			if (val != null) {
+				params.namespace = val;
+			}
+			val = f.getElementValue("/node()/*[local-name(.) = 'modules-database']");
+			if (val != null) {
+				params.modulesDatabase = val;
+			}
+		}
+		return params;
+	}
 
-    public void setDocumentUri(String documentUri) {
-        this.documentUri = documentUri;
-    }
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+	}
 
-    public void setModulesDatabase(String modulesDatabase) {
-        this.modulesDatabase = modulesDatabase;
-    }
+	public void setDocumentUri(String documentUri) {
+		this.documentUri = documentUri;
+	}
+
+	public void setModulesDatabase(String modulesDatabase) {
+		this.modulesDatabase = modulesDatabase;
+	}
+}
+
+class AmpParams {
+	public String documentUri;
+	public String modulesDatabase;
+	public String namespace;
 }
--- src/test/java/com/marklogic/appdeployer/command/security/ManageAmpsTest.java
+++ src/test/java/com/marklogic/appdeployer/command/security/ManageAmpsTest.java
@@ -2,24 +2,80 @@ package com.marklogic.appdeployer.command.security;
 
 import com.marklogic.appdeployer.command.AbstractManageResourceTest;
 import com.marklogic.appdeployer.command.Command;
+import com.marklogic.mgmt.ManageClient;
 import com.marklogic.mgmt.ResourceManager;
 import com.marklogic.mgmt.security.AmpManager;
+import org.junit.Test;
 
 public class ManageAmpsTest extends AbstractManageResourceTest {
 
-    @Override
-    protected ResourceManager newResourceManager() {
-        return new AmpManager(manageClient);
-    }
+	/**
+	 * This test verifies that AmpManager can correctly handle two amps that have the same local-name, document-uri,
+	 * and namespace, but with a different modules-database.
+	 *
+	 * Note that the module doesn't actually have to exist in order to create an amp.
+	 */
+	@Test
+	public void twoAmpsWithDifferentModulesDatabase() {
+		String amp1 = "{\n" +
+			"  \"namespace\": \"http://example.com/uri\",\n" +
+			"  \"local-name\": \"ml-app-deployer-test-amp\",\n" +
+			"  \"document-uri\": \"/module/path/name\",\n" +
+			"  \"modules-database\": \"Documents\",\n" +
+			"  \"role\": [\"rest-writer\"]\n" +
+			"}";
 
-    @Override
-    protected Command newCommand() {
-        return new DeployAmpsCommand();
-    }
+		String amp2 = "{\n" +
+			"  \"namespace\": \"http://example.com/uri\",\n" +
+			"  \"local-name\": \"ml-app-deployer-test-amp\",\n" +
+			"  \"document-uri\": \"/module/path/name\",\n" +
+			"  \"modules-database\": \"Modules\",\n" +
+			"  \"role\": [\"rest-writer\"]\n" +
+			"}";
 
-    @Override
-    protected String[] getResourceNames() {
-        return new String[] { "sample-app-amp-1" };
-    }
+		ManageClient client = new ManageClient();
+		AmpManager mgr = new AmpManager(client);
+
+		try {
+			// Create and verify
+			mgr.save(amp1);
+			mgr.save(amp2);
+			assertTrue(mgr.ampExists(amp1));
+			assertTrue(mgr.ampExists(amp2));
+
+			// Update and verify
+			mgr.save(amp1);
+			mgr.save(amp2);
+			assertTrue(mgr.ampExists(amp1));
+			assertTrue(mgr.ampExists(amp2));
+		} finally {
+			// Delete and verify
+			mgr.delete(amp1);
+			mgr.delete(amp2);
+			assertFalse(mgr.ampExists(amp1));
+			assertFalse(mgr.ampExists(amp2));
+		}
+	}
+
+	@Override
+	protected ResourceManager newResourceManager() {
+		return new AmpManager(manageClient);
+	}
+
+	@Override
+	protected Command newCommand() {
+		return new DeployAmpsCommand();
+	}
+
+	/**
+	 * The second amp doesn't have a modules database specified, so we can verify the amp can still be
+	 * created/deleted when it refers to a filesystem module.
+	 * 
+	 * @return
+	 */
+	@Override
+	protected String[] getResourceNames() {
+		return new String[]{"ml-app-deployer-test-1", "ml-app-deployer-test-2"};
+	}
 
 }

0d8dd2f4c8449542f952b9c6214ec9cac5e7c90e
Rob Rudin <rjrudin@gmail.com>
2016-11-12 20:25:12 -0500

    Updating assertion to not depend on specific order

--- src/test/java/com/marklogic/client/schemasloader/impl/DefaultSchemasLoaderTest.java
+++ src/test/java/com/marklogic/client/schemasloader/impl/DefaultSchemasLoaderTest.java
@@ -1,9 +1,11 @@
 package com.marklogic.client.schemasloader.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -19,12 +21,16 @@ public class DefaultSchemasLoaderTest {
 	public void testDefaultSchemasLoaderTest() {
 		finder = new DefaultSchemasFinder();
 		List<File> files = finder.findSchemas(getBaseDir("sample-base-dir/schemas"));
-		
+
 		assertEquals("Found 3 files", 3, files.size());
-		
-		assertEquals("File Item 0", "my.ruleset", files.get(0).getName());
-		assertEquals("File Item 1", "my.tde", files.get(1).getName());
-		assertEquals("File Item 2", "my.xsd", files.get(2).getName());
+
+		List<String> names = new ArrayList<>();
+		for (File f : files) {
+			names.add(f.getName());
+		}
+		assertTrue(names.contains("my.ruleset"));
+		assertTrue(names.contains("my.tde"));
+		assertTrue(names.contains("my.xsd"));
 	}
 
 	private File getBaseDir(String path) {

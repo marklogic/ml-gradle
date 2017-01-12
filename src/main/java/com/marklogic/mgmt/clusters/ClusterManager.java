b3ab36eae374fb4e54362fe2dd449d4d019cf3de
Chris Keefer <ckeefer@gmail.com>
2017-01-11 21:22:08 -0500

    Added support to add and remove hosts from a cluster

--- src/main/java/com/marklogic/mgmt/clusters/ClusterManager.java
+++ src/main/java/com/marklogic/mgmt/clusters/ClusterManager.java
@@ -3,6 +3,7 @@ package com.marklogic.mgmt.clusters;
 import com.marklogic.mgmt.AbstractManager;
 import com.marklogic.mgmt.ManageClient;
 import com.marklogic.mgmt.admin.ActionRequiringRestart;
+import com.marklogic.mgmt.admin.AdminConfig;
 import com.marklogic.mgmt.admin.AdminManager;
 import com.marklogic.rest.util.Fragment;
 
@@ -35,4 +36,68 @@ public class ClusterManager extends AbstractManager {
     	Fragment f = manageClient.getXml("/manage/v2");
 	    return f.getElementValue("/c:local-cluster-default/c:version");
     }
+
+	/**
+	 * Adds a host to the cluster represented by adminManager
+	 * @param adminManager AdminManager for a host already in the cluster
+	 * @param hostname hostname of the joining host
+	 * @param username admin username of the joining host
+	 * @param password admin password of the joining host
+	 * @param group name of the group within the cluster to add the joining host to
+	 * @throws Exception
+	 */
+    public void addHost(AdminManager adminManager, String hostname, String username, String password, String group) throws Exception {
+		addHost(adminManager, hostname, username, password, group, null);
+	}
+
+	/**
+	 * Adds a host to the cluster represented by adminManager
+	 * @param adminManager AdminManager for a host already in the cluster
+	 * @param hostname hostname of the joining host
+	 * @param username admin username of the joining host
+	 * @param password admin password of the joining host
+	 * @param group name of the group within the cluster to add the joining host to
+	 * @param zone zone information for the joining host (optional)
+	 * @throws Exception
+	 */
+    public void addHost(AdminManager adminManager, String hostname, String username, String password, String group, String zone) throws Exception{
+		AdminConfig joiningHostAdminConfig = new AdminConfig(hostname, 8001, username, password);
+		AdminManager joiningHostAdminManager = new AdminManager(joiningHostAdminConfig);
+
+		// get the joining host's configuration
+		Fragment fragment = joiningHostAdminManager.getServerConfig();
+
+		// Make sure host has previously been initialized. If it hasn't, the host-id from the getServerConfig will
+		// be empty
+		String hostId = fragment.getElementValue("node()/m:host-id");
+		if(hostId.isEmpty()){
+			logger.error("New host [" + hostname + "] has not been initialized. Please initialize the host first.");
+			return;
+		}
+
+		// Send the joining host's config to the bootstrap host and receive
+		// the cluster config data needed to complete the join.
+		byte[] clusterConfigZipBytes = adminManager.postJoiningHostConfig(fragment, group, zone);
+		if(clusterConfigZipBytes == null){
+			logger.error("Error sending new host's config to cluster and receiving updated cluster configuration");
+			return;
+		}
+
+		// Final step of adding a host to the cluster. Post the zipped cluster config to the
+		// joining host
+		joiningHostAdminManager.postClustConfigToJoiningHost(clusterConfigZipBytes);
+
+	}
+
+	/**
+	 * Removes the specified host from the cluster. Assumes connection information is of the host to remove
+	 * @param hostname hostname of the server to remove from the cluster
+	 */
+	public void removeHost(String hostname){
+		AdminConfig adminConfig = new AdminConfig(hostname, 8001, manageClient.getManageConfig().getUsername(), manageClient.getManageConfig().getPassword());
+		AdminManager adminManager = new AdminManager(adminConfig);
+		adminManager.leaveCluster();
+
+	}
+
 }
---
+++ src/test/java/com/marklogic/mgmt/clusters/AddAndRemoveHostDebug.java
@@ -0,0 +1,51 @@
+package com.marklogic.mgmt.clusters;
+
+import com.marklogic.mgmt.ManageClient;
+import com.marklogic.mgmt.ManageConfig;
+import com.marklogic.mgmt.admin.AdminConfig;
+import com.marklogic.mgmt.admin.AdminManager;
+import com.marklogic.mgmt.hosts.HostManager;
+import org.junit.Assert;
+
+/**
+ * Basic test to test adding a new host to a cluster and then removing it.
+ *
+ * Note: The new host must have already been initialized
+ * Note: After running this test, the new host will have been removed from the cluster and
+ *       will need to be re-initialized before this test can be run again.
+ */
+public class AddAndRemoveHostDebug {
+
+	public static void main(String[] args) throws Exception{
+		String existingHost = args[0];
+		String existingHostPassword = args[1];
+		String joiningHost = args[2];
+		String joiningHostPassword = args[3];
+		String group = "Default";
+		String zone = "Don't let me get in my zone";
+
+		ManageConfig manageConfig = new ManageConfig(existingHost, 8002, "admin", existingHostPassword);
+		ManageClient manageClient = new ManageClient(manageConfig);
+
+		HostManager hostManager = new HostManager(manageClient);
+
+		Assert.assertEquals("Expecting only one host to start out with", 1, hostManager.getHostIds().size());
+
+		// Add joining host to the cluster
+		ClusterManager clusterManager = new ClusterManager(manageClient);
+		AdminConfig adminConfig = new AdminConfig(existingHost, 8001, "admin", existingHostPassword);
+		AdminManager adminManager = new AdminManager(adminConfig);
+		clusterManager.addHost(adminManager, joiningHost, "admin", joiningHostPassword, "Default");
+
+		Assert.assertEquals("Expecting two hosts after the add", 2, hostManager.getHostIds().size());
+		System.out.println("After adding a host, the cluster now has two hosts");
+
+		// Remove joining host from cluster
+		clusterManager.removeHost(joiningHost);
+
+		Assert.assertEquals("Expecting 1 host after removing", 1, hostManager.getHostIds().size());
+		System.out.println("After removing a host, the cluster now has one host");
+
+
+	}
+}

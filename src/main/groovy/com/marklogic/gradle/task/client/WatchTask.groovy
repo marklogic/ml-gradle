7ce83918e07e7107fed7ee1cb99b8e9957a8294a
Rob Rudin <robert.rudin2@boeing.com>
2017-01-27 08:59:57 -0500

    Not shutting down the new task executor after loading modules

--- src/main/groovy/com/marklogic/gradle/task/client/WatchTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/client/WatchTask.groovy
@@ -12,41 +12,43 @@ import com.marklogic.gradle.task.MarkLogicTask
 /**
  * Runs an infinite loop, and each second, it loads any new/modified modules. Often useful to run with the Gradle "-i" flag
  * so you can see which modules are loaded.
- * 
+ *
  * Depends on an instance of LoadModulesCommand being in the Gradle Project, which should have been placed there by
  * MarkLogicPlugin. This prevents this class from having to know how to construct a ModulesLoader.
  */
 class WatchTask extends MarkLogicTask {
 
-    long sleepTime = 1000
+	long sleepTime = 1000
 
-    @TaskAction
-    public void watchModules() {
-        LoadModulesCommand command = getProject().property("mlLoadModulesCommand")
-        
-        // Adjust the loader to catch exceptions by default
-        ModulesLoader loader = command.getModulesLoader()
-        if (loader == null) {
-            command.initializeDefaultModulesLoader(getCommandContext())
-            loader = command.getModulesLoader()
-        }
-        if (loader instanceof DefaultModulesLoader) {
-            ((DefaultModulesLoader)loader).setCatchExceptions(true)
-        }
-        
-        List<String> paths = getAppConfig().getModulePaths()
-        println "Watching modules in paths: " + paths
+	@TaskAction
+	void watchModules() {
+		LoadModulesCommand command = getProject().property("mlLoadModulesCommand")
 
-        DatabaseClient client = newClient()
-        while (true) {
-            for (String path : paths) {
-                loader.loadModules(new File(path), new DefaultModulesFinder(), client);
-            }
-            try {
-                Thread.sleep(sleepTime);
-            } catch (InterruptedException ie) {
-                // Ignore
-            }
-        }
-    }
+		ModulesLoader loader = command.getModulesLoader()
+		if (loader == null) {
+			command.initializeDefaultModulesLoader(getCommandContext())
+			loader = command.getModulesLoader()
+		}
+
+		if (loader instanceof DefaultModulesLoader) {
+			DefaultModulesLoader dml = (DefaultModulesLoader) loader;
+			dml.setCatchExceptions(true)
+			dml.setShutdownTaskExecutorAfterLoadingModules(false)
+		}
+
+		List<String> paths = getAppConfig().getModulePaths()
+		println "Watching modules in paths: " + paths
+
+		DatabaseClient client = newClient()
+		while (true) {
+			for (String path : paths) {
+				loader.loadModules(new File(path), new DefaultModulesFinder(), client);
+			}
+			try {
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException ie) {
+				// Ignore
+			}
+		}
+	}
 }

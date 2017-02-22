7d0e60e2ae32301a00ea3efe1c5e1b1526ef2e63
Rob Rudin <robert.rudin2@boeing.com>
2017-02-22 11:48:51 -0500

    #135 Little bit of logging for loading schemas

--- src/main/java/com/marklogic/appdeployer/command/schemas/LoadSchemasCommand.java
+++ src/main/java/com/marklogic/appdeployer/command/schemas/LoadSchemasCommand.java
@@ -6,7 +6,6 @@ import com.marklogic.appdeployer.command.CommandContext;
 import com.marklogic.appdeployer.command.SortOrderConstants;
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.FailedRequestException;
-import com.marklogic.client.schemasloader.SchemasLoader;
 import com.marklogic.client.schemasloader.impl.DefaultSchemasLoader;
 
 public class LoadSchemasCommand extends AbstractCommand {
@@ -23,11 +22,12 @@ public class LoadSchemasCommand extends AbstractCommand {
 	protected void loadSchemasIntoSchemasDatabase(CommandContext context) {
 		AppConfig config = context.getAppConfig();
 		DatabaseClient client = config.newSchemasDatabaseClient();
-		SchemasLoader schemasLoader = new DefaultSchemasLoader(client);
+		DefaultSchemasLoader schemasLoader = new DefaultSchemasLoader(client);
 		try {
 			String schemasPath = config.getSchemasPath();
-			logger.info("Loading schemas database data from path: " + schemasPath);
+			logger.info("Loading schemas from path: " + schemasPath);
 			schemasLoader.loadSchemas(schemasPath);
+			logger.info("Finished loading schemas from: " + schemasPath);
 		} catch (FailedRequestException fre) {
 			if (fre.getMessage().contains("NOSUCHDB")) {
 				logger.warn("Unable to load schemas because no schemas database exists; cause: " + fre.getMessage());

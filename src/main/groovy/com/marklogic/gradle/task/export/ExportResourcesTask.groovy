d4f488018192654fa60cb5682da2ec79e06f2bc1
Rob Rudin <robert.rudin2@boeing.com>
2017-05-09 09:24:01 -0400

    Making it easier to select types to include for exporting

--- src/main/groovy/com/marklogic/gradle/task/export/ExportResourcesTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/export/ExportResourcesTask.groovy
@@ -20,7 +20,7 @@ class ExportResourcesTask extends MarkLogicTask {
 		String includeTypesPropName = "includeTypes"
 		String includeTypes = null
 		if (getProject().hasProperty(includeTypesPropName)) {
-			includeTypes = getProject().property(includeTypesPropName).split(",")
+			includeTypes = getProject().property(includeTypesPropName)
 		}
 
 		if (getProject().hasProperty(filePropName)) {
@@ -34,26 +34,26 @@ class ExportResourcesTask extends MarkLogicTask {
 		} else if (getProject().hasProperty(prefixPropName)) {
 			String prefix = getProject().property(prefixPropName)
 			PrefixResourceSelector selector = new PrefixResourceSelector(prefix)
-			if (includeTypes != null) {
-				selector.setIncludeTypes(includeTypes)
-			}
+			selector.setIncludeTypesAsString(includeTypes)
 			export(selector)
 		} else if (getProject().hasProperty(regexPropName)) {
 			String regex = getProject().property(regexPropName)
 			RegexResourceSelector selector = new RegexResourceSelector(regex)
-			if (includeTypes != null) {
-				selector.setIncludeTypes(includeTypes)
-			}
+			selector.setIncludeTypesAsString(includeTypes)
 			export(selector)
 		} else {
-			println "Use -PpropertiesFile or -Pprefix or -Pregex to specify the resources to export, with -PincludeTypes for restricting the types to export when using -Pprefix or -Pregex"
+			println "Use -PpropertiesFile or -Pprefix or -Pregex to specify the resources to export, with -PincludeTypes=comma-delimited-string for restricting the types to export when using -Pprefix or -Pregex, and -PexportPath to customize where to export the resources to"
 		}
 	}
 
 	void export(ResourceSelector selector) {
-		ExportedResources resources = new Exporter(getManageClient()).
-			select(selector).
-			export(getAppConfig().getConfigDir().getBaseDir())
+		def path = getAppConfig().getConfigDir().getBaseDir()
+		if (getProject().hasProperty("exportPath")) {
+			path = new File(getProject().property("exportPath"))
+		}
+		println "Exporting resources to: " + path
+
+		ExportedResources resources = new Exporter(getManageClient()).select(selector).export(path)
 		println "Exported files:"
 		for (File f : resources.getFiles()) {
 			println f.getAbsolutePath()

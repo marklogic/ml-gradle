642c593751e444e216beeeed893bd079c1c90723
Rob Rudin <rob.rudin@marklogic.com>
2017-08-07 13:06:50 -0400

    Bumped to 2.14.1, fixed test

--- src/test/java/com/marklogic/client/util/DocumentFinderTest.java
+++ src/test/java/com/marklogic/client/util/DocumentFinderTest.java
@@ -17,7 +17,7 @@ public class DocumentFinderTest extends Assert {
 	public void noFileFilter() {
 		String path = "src/test/resources/schemas";
 		List<DocumentFile> list = sut.readDocumentFiles(path);
-		assertEquals(3, list.size());
+		assertEquals(5, list.size());
 
 		List<String> uris = new ArrayList<>();
 		for (DocumentFile file : list) {
@@ -26,5 +26,7 @@ public class DocumentFinderTest extends Assert {
 		assertTrue(uris.contains("/child/child.tdej"));
 		assertTrue(uris.contains("/child/grandchild/grandchild.tdex"));
 		assertTrue(uris.contains("/parent.tdex"));
+		assertTrue(uris.contains("/not-tde/ruleset.txt"));
+		assertTrue(uris.contains("/tde/ruleset.txt"));
 	}
 }

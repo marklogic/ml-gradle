c8993306eb9fb9f1c267e8e852491629898b2082
Dermot Smyth <Dermot.Smyth@marklogic.com>
2017-09-13 07:55:20 +0200

    added uni-temporal examples

---
+++ src/test/java/com/marklogic/appdeployer/command/temporal/DeployUniTemporalTest.java
@@ -0,0 +1,41 @@
+package com.marklogic.appdeployer.command.temporal;
+
+import com.marklogic.appdeployer.AbstractAppDeployerTest;
+import com.marklogic.appdeployer.command.databases.DeployContentDatabasesCommand;
+import com.marklogic.appdeployer.command.databases.DeployOtherDatabasesCommand;
+import com.marklogic.appdeployer.command.databases.DeploySchemasDatabaseCommand;
+import com.marklogic.mgmt.resource.temporal.TemporalAxesManager;
+import com.marklogic.mgmt.resource.temporal.TemporalCollectionManager;
+import com.marklogic.rest.util.ResourcesFragment;
+import org.junit.Test;
+
+import java.io.File;
+
+public class DeployUniTemporalTest extends AbstractAppDeployerTest {
+
+	@Test
+	public void test() {
+		appConfig.getConfigDir().setBaseDir(new File("src/test/resources/sample-app/uni-temporal-config"));
+
+		initializeAppDeployer(new DeployContentDatabasesCommand(1),
+			new DeploySchemasDatabaseCommand(), new DeployTemporalAxesCommand(),
+			new DeployTemporalCollectionsCommand(), new DeployTemporalCollectionsLSQTCommand(),
+			new DeployOtherDatabasesCommand());
+
+		try {
+			appDeployer.deploy(appConfig);
+
+			verifyTemporalDocsExist(appConfig.getContentDatabaseName());
+		} finally {
+			undeploySampleApp();
+		}
+	}
+
+	private void verifyTemporalDocsExist(String databaseName) {
+		ResourcesFragment axes = new TemporalAxesManager(manageClient, databaseName).getAsXml();
+		assertEquals(1, axes.getResourceCount());
+
+		ResourcesFragment collections = new TemporalCollectionManager(manageClient, databaseName).getAsXml();
+		assertEquals(1, collections.getResourceCount());
+	}
+}

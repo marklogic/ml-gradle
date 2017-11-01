0a446e121cb0d0a573639f34ec252adea1e6a755
Rob Rudin <rob.rudin@marklogic.com>
2017-10-31 21:00:43 -0400

    #231 mlLoadModules now deletes test modules
    And mlReloadModules as well

--- src/main/groovy/com/marklogic/gradle/task/client/LoadModulesTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/client/LoadModulesTask.groovy
@@ -1,5 +1,6 @@
 package com.marklogic.gradle.task.client
 
+import com.marklogic.appdeployer.command.modules.DeleteTestModulesCommand
 import org.gradle.api.tasks.TaskAction
 
 import com.marklogic.appdeployer.AppDeployer
@@ -9,18 +10,27 @@ import com.marklogic.gradle.task.MarkLogicTask
 
 class LoadModulesTask extends MarkLogicTask {
 
-    @TaskAction
-    void loadModules() {
-        LoadModulesCommand command = null
-        // Check for a LoadModulesCommand in the AppDeployer first, as that may have additional configuration that we
-        // don't want to have to duplicate on this task
-        AppDeployer d = getAppDeployer()
-        if (d instanceof SimpleAppDeployer) {
-            command = d.getCommand("LoadModulesCommand")
-        }
-        if (command == null) {
-            command = new LoadModulesCommand()
-        }
-        command.execute(getCommandContext())
-    }
+	@TaskAction
+	void loadModules() {
+		LoadModulesCommand command = null
+		DeleteTestModulesCommand deleteCommand = null
+
+		// Check for a LoadModulesCommand in the AppDeployer first, as that may have additional configuration that we
+		// don't want to have to duplicate on this task
+		AppDeployer d = getAppDeployer()
+		if (d instanceof SimpleAppDeployer) {
+			command = d.getCommand("LoadModulesCommand")
+			deleteCommand = d.getCommand("DeleteTestModulesCommand")
+		}
+
+		if (command == null) {
+			command = new LoadModulesCommand()
+		}
+		if (deleteCommand == null) {
+			deleteCommand = new DeleteTestModulesCommand()
+		}
+
+		command.execute(getCommandContext())
+		deleteCommand.execute(getCommandContext())
+	}
 }

f7551e460cec5ae91f7e13a065940b15f32ae60c
Phil Barber <Phil.Barber@marklogic.com>
2018-02-19 10:27:31 -0500

    Lots of changes. Renamed the class and the task. Simplified all samples. Improved comments and logging.

--- src/main/groovy/com/marklogic/gradle/task/roxy/RoxyGenerateTestSuiteTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/roxy/RoxyGenerateTestSuiteTask.groovy
@@ -2,27 +2,24 @@ package com.marklogic.gradle.task.roxy
 
 import org.gradle.api.tasks.TaskAction
 
-//import com.marklogic.gradle.task.roxy.scaffoldTemplates.SampleTestFiles
-
-// Task mlGenerateRoxyTest : This task uses Groovy's SimpleTemplateEngine to customize the sample test suite
+// Task mlRoxyGenerateTestSuite : This task uses Groovy's SimpleTemplateEngine to customize the sample test suite
 // Parameters:
 //    suiteName       (Default: SampleTestSuite)
 //        - Specify the name of the generated test suite
 //    testName        (Default: SampleTest)
 //        - Specify the name of the generated test
 // Examples:
-//    gradle mlGenerateRoxyTest
-//    gradle mlGenerateRoxyTest -PsuiteName="foo" -PtestName="bar"
-class RoxyGenerateTestScaffoldTask extends RoxyTask {
+//    gradle mlRoxyGenerateTestSuite
+//    gradle mlRoxyGenerateTestSuite -PsuiteName="foo" -PtestName="bar"
+class RoxyGenerateTestSuiteTask extends RoxyTask {
     CommandLineArguments arguments
 
     @TaskAction
-    void generateScaffoldTask() {
-        println "Generate Roxy Test Scaffold"
-        
+    void generateTestSuite() {
         this.arguments = new CommandLineArguments()
         
         scaffoldFromTemplates()
+		println "Finished generating Roxy test suite."
     }
 
     def scaffoldFromTemplates() {	    
@@ -38,30 +35,29 @@ class RoxyGenerateTestScaffoldTask extends RoxyTask {
         processTemplateString(engine, binding, arguments.targetSuiteDirName, "/suite-teardown.xqy", suiteTeardownXqyString)
         def setupXqyString = SampleTestFiles.getSetupXqy()
         processTemplateString(engine, binding, arguments.targetSuiteDirName, "/setup.xqy", setupXqyString)
+        def teardownXqyString = SampleTestFiles.getTeardownXqy()
+        processTemplateString(engine, binding, arguments.targetSuiteDirName, "/teardown.xqy", teardownXqyString)
     }
 
     def processTemplateString(engine, binding, targetBaseDir, templateFilePath, templateString) {
         def template = engine.createTemplate(templateString).make(binding)
         def templateResult = template.toString()
-        project.file(targetBaseDir + templateFilePath).write(templateResult)
-    }
-
-    class DefaultValues {
-        static String defaultSuiteName = 'SampleTestSuite'
-        static String templateDirName = 'test-templates'
+        def targetFilepath = targetBaseDir + templateFilePath
+        project.file(targetFilepath).write(templateResult)
+		println "Generated test suite file: " + targetFilepath
     }
 
     class CommandLineArguments {
-        String roxySuitesDirName = getAppConfig().getModulePaths().last() + "/tests"
-        String suiteName = DefaultValues.defaultSuiteName
-        String targetSuiteDirName = roxySuitesDirName + '/' + suiteName
+        String defaultSuitesDirName = "src/test/ml-modules"
+        String suiteName = 'SampleTestSuite'
+        String targetSuiteDirName = defaultSuitesDirName + '/' + suiteName
         String testName = 'SampleTest'
 
         CommandLineArguments() {
             if (project.hasProperty('suiteName')) {
                 this.suiteName = project.property('suiteName')
             }
-            this.targetSuiteDirName = this.roxySuitesDirName + '/' + suiteName
+            this.targetSuiteDirName = this.defaultSuitesDirName + '/' + suiteName
             if (project.hasProperty('testName')) {
                 this.testName = project.property('testName')
             }
--- src/main/groovy/com/marklogic/gradle/task/roxy/scaffoldTemplates/SampleTestFiles.groovy
+++ src/main/groovy/com/marklogic/gradle/task/roxy/scaffoldTemplates/SampleTestFiles.groovy
@@ -16,18 +16,36 @@ test:assert-true(fn:true())
     static String getSetupXqy() {
         return """xquery version '1.0-ml';
 
-(: Put test-specific setup code in here. Such as putting the database in a testable state. :)
-(: If no test-specific setup is required, this file may be deleted. :)
-(: Each setup runs in its own transaction. :) 
+(: 
+   This module will be run before each test in your suite.
+   Here you might insert a document into the test database that each of your tests will modify
+   If no test-specific setup is required, this file may be deleted.
+   Each setup runs in its own transaction.
+:) 
 xdmp:log("Setup COMPLETE....")
+		       """
+	}
+	
+    static String getTeardownXqy() {
+        return """xquery version '1.0-ml';
+
+(:
+   This module will run after each test in your suite.
+   You might use this module to remove the document inserted by setup.xqy/setup.sjs
+   If no suite-specific teardown is required, this file may be deleted.
+:)
+xdmp:log("Teardown COMPLETE....")
 		       """
 	}
         
     static String getSuiteSetupXqy() {
         return """xquery version '1.0-ml';
 
-(: Put suite-specific setup code in here. :)
-(: If no suite-specific setup is required, this file may be deleted. :)
+(:
+   Runs once when your suite is started.
+   You can use this to insert some data that will not be modified over the course of the suite's tests.
+   If no suite-specific setup is required, this file may be deleted.
+:)
 xdmp:log("Suite Setup COMPLETE....")
                """
     }
@@ -35,9 +53,10 @@ xdmp:log("Suite Setup COMPLETE....")
     static String getSuiteTeardownXqy() {
         return """xquery version '1.0-ml';
 
-(: Put suite-specific teardown code in here. :)
-(: This might include database cleanup. :)
-(: If no suite-specific teardown is required, this file may be deleted. :)
+(:
+   Runs once when your suite is finished, to clean up after the suite's tests.
+   If no suite-specific teardown is required, this file may be deleted.
+:)
 xdmp:log("Suite Teardown ENDING....")
                """
     }

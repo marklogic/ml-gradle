3d56762f6e1dce8766d262e4b2363e55da6ccf02
Rob Rudin <rjrudin@gmail.com>
2018-03-06 17:14:40 -0500

    #93 Moved DMSDK code to marklogic-data-movement-components

--- src/main/java/com/marklogic/client/ext/spring/SpringDatabaseClientConfig.java
+++ src/main/java/com/marklogic/client/ext/spring/SpringDatabaseClientConfig.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+/**
+ * Extends DatabaseClientConfig to apply 4 Value annotations for host/port/username/password.
+ */
 @Component
 public class SpringDatabaseClientConfig extends DatabaseClientConfig {
 
-    @Autowired
-    public SpringDatabaseClientConfig(
-            @Value("${marklogic.host}") String host,
-            @Value("${marklogic.port}") int port,
-            @Value("${marklogic.username}") String username,
-            @Value("${marklogic.password}") String password) {
-        super(host, port, username, password);
-    }
-
+	@Autowired
+	public SpringDatabaseClientConfig(
+		@Value("${marklogic.host}") String host,
+		@Value("${marklogic.port}") int port,
+		@Value("${marklogic.username}") String username,
+		@Value("${marklogic.password}") String password) {
+		super(host, port, username, password);
+	}
 
 }
--- src/test/java/com/marklogic/client/ext/AbstractIntegrationTest.java
+++ src/test/java/com/marklogic/client/ext/AbstractIntegrationTest.java
@@ -1,18 +1,23 @@
 package com.marklogic.client.ext;
 
 import com.marklogic.client.DatabaseClient;
-import com.marklogic.client.ext.spring.config.MarkLogicApplicationContext;
+import com.marklogic.client.ext.spring.SpringDatabaseClientConfig;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration( classes = {MarkLogicApplicationContext.class} )
+@ContextConfiguration(classes = {TestConfig.class})
 public abstract class AbstractIntegrationTest extends Assert {
 
 	protected Logger logger = LoggerFactory.getLogger(getClass());
@@ -45,5 +50,19 @@ public abstract class AbstractIntegrationTest extends Assert {
 		client = configuredDatabaseClientFactory.newDatabaseClient(clientConfig);
 		clientConfig.setDatabase(currentDatabase);
 		return client;
+	}
+}
+
+@Configuration
+@Import(value = {SpringDatabaseClientConfig.class})
+@PropertySource("classpath:application.properties")
+class TestConfig {
+
+	/**
+	 * Ensures that placeholders are replaced with property values
+	 */
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
 	}
 }

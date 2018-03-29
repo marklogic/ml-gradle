4ce36396b907dd0d5a2807eb06d4beba07970e62
Rob Rudin <rjrudin@gmail.com>
2018-03-28 22:21:30 -0400

    #261 Can now deploy triggers to any database

--- src/main/java/com/marklogic/appdeployer/command/triggers/DeployTriggersCommand.java
+++ src/main/java/com/marklogic/appdeployer/command/triggers/DeployTriggersCommand.java
@@ -1,5 +1,7 @@
 package com.marklogic.appdeployer.command.triggers;
 
+import com.marklogic.appdeployer.AppConfig;
+import com.marklogic.appdeployer.ConfigDir;
 import com.marklogic.appdeployer.command.AbstractResourceCommand;
 import com.marklogic.appdeployer.command.CommandContext;
 import com.marklogic.appdeployer.command.SortOrderConstants;
@@ -11,10 +13,13 @@ import java.io.File;
 /**
  * Defaults to the triggers database name in the AppConfig instance. Can be overridden via the databaseNameOrId
  * property.
+ *
+ * As of version 3.7.0, this now supports a triggers "database resource directory" under "databases/(name of triggers database)/triggers".
  */
 public class DeployTriggersCommand extends AbstractResourceCommand {
 
     private String databaseIdOrName;
+    private TriggerManager currentTriggerManager;
 
     public DeployTriggersCommand() {
         setExecuteSortOrder(SortOrderConstants.DEPLOY_TRIGGERS);
@@ -22,15 +27,31 @@ public class DeployTriggersCommand extends AbstractResourceCommand {
         setDeleteResourcesOnUndo(false);
     }
 
-    @Override
+	@Override
+	public void execute(CommandContext context) {
+		AppConfig appConfig = context.getAppConfig();
+		for (ConfigDir configDir : appConfig.getConfigDirs()) {
+			final String initialTriggersDatabaseName = databaseIdOrName != null ? databaseIdOrName : appConfig.getTriggersDatabaseName();
+			deployTriggers(context, configDir, initialTriggersDatabaseName);
+			for (File databaseResourceDir : configDir.getDatabaseResourceDirectories()) {
+				deployTriggers(context, new ConfigDir(databaseResourceDir), databaseResourceDir.getName());
+			}
+		}
+	}
+
+	protected void deployTriggers(CommandContext context, ConfigDir configDir, String databaseIdOrName) {
+		currentTriggerManager = new TriggerManager(context.getManageClient(), databaseIdOrName);
+		processExecuteOnResourceDir(context, configDir.getTriggersDir());
+	}
+
+	@Override
+	protected ResourceManager getResourceManager(CommandContext context) {
+		return currentTriggerManager;
+	}
+
+	@Override
     protected File[] getResourceDirs(CommandContext context) {
     	return findResourceDirs(context, configDir -> configDir.getTriggersDir());
-    }
-
-    @Override
-    protected ResourceManager getResourceManager(CommandContext context) {
-        String db = databaseIdOrName != null ? databaseIdOrName : context.getAppConfig().getTriggersDatabaseName();
-        return new TriggerManager(context.getManageClient(), db);
     }
 
     public void setDatabaseIdOrName(String databaseIdOrName) {
---
+++ src/test/java/com/marklogic/appdeployer/command/databases/DeployTriggersToMultipleDatabasesTest.java
@@ -0,0 +1,34 @@
+package com.marklogic.appdeployer.command.databases;
+
+import com.marklogic.appdeployer.AbstractAppDeployerTest;
+import com.marklogic.appdeployer.ConfigDir;
+import com.marklogic.appdeployer.command.triggers.DeployTriggersCommand;
+import com.marklogic.mgmt.resource.triggers.TriggerManager;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.File;
+
+public class DeployTriggersToMultipleDatabasesTest extends AbstractAppDeployerTest {
+
+	@After
+	public void teardown() {
+		undeploySampleApp();
+	}
+
+	@Test
+	public void test() {
+		appConfig.setConfigDir(new ConfigDir(new File("src/test/resources/sample-app/multiple-triggers-databases")));
+
+		initializeAppDeployer(new DeployContentDatabasesCommand(1), new DeployTriggersDatabaseCommand(),
+			new DeployOtherDatabasesCommand(), new DeployTriggersCommand());
+
+		deploySampleApp();
+
+		TriggerManager triggerManager = new TriggerManager(manageClient, appConfig.getTriggersDatabaseName());
+		assertTrue(triggerManager.exists("my-trigger"));
+
+		triggerManager = new TriggerManager(manageClient, "other-" + appConfig.getTriggersDatabaseName());
+		assertTrue(triggerManager.exists("other-trigger"));
+	}
+}

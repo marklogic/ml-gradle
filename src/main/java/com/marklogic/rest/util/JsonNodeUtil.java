c5925bc5b945015046d887376bc3eba3829f9d9e
Rob Rudin <rob.rudin@marklogic.com>
2018-11-20 20:00:15 -0500

    #315 Extracted mergeObjectNodes

--- src/main/java/com/marklogic/rest/util/JsonNodeUtil.java
+++ src/main/java/com/marklogic/rest/util/JsonNodeUtil.java
@@ -10,49 +10,61 @@ import java.util.*;
 
 public class JsonNodeUtil {
 
-    public static JsonNode mergeJsonFiles(List<File> files) {
-        List<JsonNode> nodes = new ArrayList<>();
-        Set<String> fieldNames = new HashSet<>();
-        for (File f : files) {
-            if (f.exists()) {
-                JsonNode node = null;
-                try {
-                    node = ObjectMapperFactory.getObjectMapper().readTree(f);
-                } catch (Exception e) {
-                    throw new RuntimeException("Unable to read JSON from file: " + f.getAbsolutePath(), e);
-                }
-                nodes.add(node);
-                Iterator<String> names = node.fieldNames();
-                while (names.hasNext()) {
-                    fieldNames.add(names.next());
-                }
-            }
-        }
+	public static JsonNode mergeJsonFiles(List<File> files) {
+		List<JsonNode> nodes = new ArrayList<>();
+		for (File f : files) {
+			if (f.exists()) {
+				ObjectNode node = null;
+				try {
+					node = (ObjectNode) ObjectMapperFactory.getObjectMapper().readTree(f);
+				} catch (Exception e) {
+					throw new RuntimeException("Unable to read JSON from file: " + f.getAbsolutePath(), e);
+				}
+				nodes.add(node);
+			}
+		}
 
-        if (nodes.isEmpty()) {
-            return null;
-        }
+		if (nodes.isEmpty()) {
+			return null;
+		}
 
-        // Merge each node back into the previous one
-        for (int i = 1; i < nodes.size(); i++) {
-            ObjectNode target = (ObjectNode) nodes.get(i);
-            ObjectNode source = (ObjectNode) nodes.get(i - 1);
-            for (String name : fieldNames) {
-                JsonNode targetField = target.get(name);
-                JsonNode sourceField = source.get(name);
-                if (sourceField == null) {
-                    continue;
-                } else if (targetField == null) {
-                    target.set(name, sourceField);
-                } else if (sourceField.isArray()) {
-                    ArrayNode sourceArray = (ArrayNode) sourceField;
-                    ArrayNode targetArray = (ArrayNode) targetField;
-                    targetArray.addAll(sourceArray);
-                }
-            }
-        }
+		return mergeObjectNodes(nodes.toArray(new ObjectNode[]{}));
+	}
 
-        return nodes.get(nodes.size() - 1);
-    }
+	public static ObjectNode mergeObjectNodes(ObjectNode... nodes) {
+		List<ObjectNode> nodeList = new ArrayList<>();
+		Set<String> fieldNames = new HashSet<>();
+		for (ObjectNode node : nodes) {
+			nodeList.add(node);
+			Iterator<String> names = node.fieldNames();
+			while (names.hasNext()) {
+				fieldNames.add(names.next());
+			}
+		}
 
+		if (nodeList.isEmpty()) {
+			return null;
+		}
+
+		// Merge each node back into the previous one
+		for (int i = 1; i < nodeList.size(); i++) {
+			ObjectNode target = nodeList.get(i);
+			ObjectNode source = nodeList.get(i - 1);
+			for (String name : fieldNames) {
+				JsonNode targetField = target.get(name);
+				JsonNode sourceField = source.get(name);
+				if (sourceField == null) {
+					continue;
+				} else if (targetField == null) {
+					target.set(name, sourceField);
+				} else if (sourceField.isArray()) {
+					ArrayNode sourceArray = (ArrayNode) sourceField;
+					ArrayNode targetArray = (ArrayNode) targetField;
+					targetArray.addAll(sourceArray);
+				}
+			}
+		}
+
+		return nodeList.get(nodeList.size() - 1);
+	}
 }

fd111978ed5a2d179678ada72a17fff77ce08761
Rob Rudin <rob.rudin@marklogic.com>
2018-11-28 13:54:01 -0500

    Fixing LoadSchemasTask to invoke all commands in the schema commands list

--- src/main/groovy/com/marklogic/gradle/task/schemas/LoadSchemasTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/schemas/LoadSchemasTask.groovy
@@ -1,26 +1,12 @@
 package com.marklogic.gradle.task.schemas
 
-import org.gradle.api.tasks.TaskAction
-
-import com.marklogic.appdeployer.AppDeployer
-import com.marklogic.appdeployer.command.schemas.LoadSchemasCommand
-import com.marklogic.appdeployer.impl.SimpleAppDeployer
 import com.marklogic.gradle.task.MarkLogicTask
+import org.gradle.api.tasks.TaskAction
 
 class LoadSchemasTask extends MarkLogicTask {
 
-    @TaskAction
-    void loadSchemas() {
-       LoadSchemasCommand command = null
-        // Check for a LoadSchemasCommand in the AppDeployer first, as that may have additional configuration that we
-        // don't want to have to duplicate on this task
-        AppDeployer d = getAppDeployer()
-        if (d instanceof SimpleAppDeployer) {
-            command = d.getCommand("LoadSchemasCommand")
-        }
-        if (command == null) {
-            command = new LoadSchemasCommand()
-        }
-        command.execute(getCommandContext())
-    }
+	@TaskAction
+	void loadSchemas() {
+		deployWithCommandListProperty("mlSchemaCommands")
+	}
 }

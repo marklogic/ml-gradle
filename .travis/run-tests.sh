9934d7b56a78409dec4011c33171819b314e2470
Rob Rudin <rob.rudin@marklogic.com>
2019-04-26 16:19:29 -0400

    Configuring Travis to just compile code for now

--- .travis/run-tests.sh
+++ .travis/run-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Can't run "test" because it exceeds the time limit for a Travis job
+# Also get intermittent 503 errors when running via Travis
+
 if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] ; then
-  ./gradlew clean test
+  ./gradlew clean compiletestjava
 fi

6df9ed2848385ff199f4514fa1503101d4e8177b
Rob Rudin <rob.rudin@marklogic.com>
2019-11-13 11:22:45 -0500

    #519 Added note to mlClearTriggersDatabase

--- src/main/groovy/com/marklogic/gradle/task/databases/ClearTriggersDatabaseTask.groovy
+++ src/main/groovy/com/marklogic/gradle/task/databases/ClearTriggersDatabaseTask.groovy
@@ -7,11 +7,13 @@ import com.marklogic.mgmt.resource.databases.DatabaseManager
 
 class ClearTriggersDatabaseTask extends MarkLogicTask {
 
-    @TaskAction
-    void clearModules() {
-        println "Clearing all documents in triggers database"
-        DatabaseManager mgr = new DatabaseManager(getManageClient())
-        mgr.clearDatabase(getAppConfig().getTriggersDatabaseName())
-        println "Finished clearing all documents in triggers database"
-    }
+	@TaskAction
+	void clearModules() {
+		println "\nConsider using mlClearDatabase instead, which provides more flexibility via -Pdatabase= for clearing any database"
+
+		println "\nClearing all documents in triggers database"
+		DatabaseManager mgr = new DatabaseManager(getManageClient())
+		mgr.clearDatabase(getAppConfig().getTriggersDatabaseName())
+		println "Finished clearing all documents in triggers database"
+	}
 }

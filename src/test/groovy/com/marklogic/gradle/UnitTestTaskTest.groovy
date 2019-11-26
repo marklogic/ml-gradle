018759b2fa3b079e718cf317506f29b10b57c044
jgardner <jgardne1@gmail.com>
2019-11-26 10:25:57 -0500

    Escape Filename Tests
    Adding a few automated test cases to show the issue that is being
    addressed.  Gradle was throwing an error when it attempted to write to
    filenames that included \ and / in the filename.  Those characters were
    being treated as new directories when gradle was writing the results to
    the file system.  The directory separators will be replaced with .
    which matches the test result filenames used by Java and Groovy tests.

---
+++ src/test/groovy/com/marklogic/gradle/UnitTestTaskTest.groovy
@@ -0,0 +1,28 @@
+package com.marklogic.gradle
+
+import org.junit.Test
+import com.marklogic.gradle.task.test.UnitTestTask
+
+class UnitTestTaskTest extends GroovyTestCase {
+
+
+	@Test
+	void testEscapingJavascriptFilenames() {
+		assertEquals("nestedTest.filename.sjs", UnitTestTask.escapeFilename("nestedTest/filename.sjs"))
+		assertEquals("nestedTest.filename.sjs", UnitTestTask.escapeFilename("nestedTest\\filename.sjs"))
+		assertEquals("nestedTest.doubleNestedTest.filenameWithDetails.sjs",
+			UnitTestTask.escapeFilename("nestedTest/doubleNestedTest/filenameWithDetails.sjs"))
+		assertEquals("nestedTest.doubleNestedTest.filenameWithDetails.sjs",
+			UnitTestTask.escapeFilename("nestedTest\\doubleNestedTest\\filenameWithDetails.sjs"))
+	}
+
+	@Test
+	public void testEscapingXqueryFilenames() {
+		assertEquals("nested-test.filename.xqy", UnitTestTask.escapeFilename("nested-test/filename.xqy"))
+		assertEquals("nested-test.filename.xqy", UnitTestTask.escapeFilename("nested-test\\filename.xqy"))
+		assertEquals("nested-test.double-nested-test.filename-with-details.xqy",
+			UnitTestTask.escapeFilename("nested-test/double-nested-test/filename-with-details.xqy"))
+		assertEquals("nested-test.double-nested-test.filename-with-details.xqy",
+			UnitTestTask.escapeFilename("nested-test\\double-nested-test\\filename-with-details.xqy"))
+	}
+}

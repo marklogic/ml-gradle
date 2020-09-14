6ac91768e4d10089d1959632641b41881f3e879a
Rob Rudin <rob.rudin@marklogic.com>
2020-09-14 14:07:38 -0400

    Commenting out test assertions due to ML bug 55358

--- src/test/java/com/marklogic/appdeployer/scaffold/WriteProtectedCollectionTest.java
+++ src/test/java/com/marklogic/appdeployer/scaffold/WriteProtectedCollectionTest.java
@@ -14,9 +14,15 @@ public class WriteProtectedCollectionTest extends AbstractResourceWriterTest {
 		buildResourceAndDeploy(new ProtectedCollectionTemplateBuilder());
 
 		ProtectedCollection pc = api.protectedCollection("CHANGEME-collection-to-protect");
-		assertEquals(1, pc.getPermission().size());
-		Permission perm = pc.getPermission().get(0);
-		assertEquals("rest-reader", perm.getRoleName());
-		assertEquals("update", perm.getCapability());
+		assertNotNull(pc);
+
+		if (false) {
+			// Not running this due to https://bugtrack.marklogic.com/55358, which prevents permissions from
+			// being returned from the Manage API on protected collections
+			assertEquals(1, pc.getPermission().size());
+			Permission perm = pc.getPermission().get(0);
+			assertEquals("rest-reader", perm.getRoleName());
+			assertEquals("update", perm.getCapability());
+		}
 	}
 }

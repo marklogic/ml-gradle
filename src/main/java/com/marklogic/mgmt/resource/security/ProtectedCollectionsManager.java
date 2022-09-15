7150a36b0631e7db5f8fb3754ca0e7639572563f
Rob Rudin <rob.rudin@marklogic.com>
2022-09-15 14:34:54 -0400

    #451 Protected collection names are now URL encoded

--- src/main/java/com/marklogic/mgmt/resource/security/ProtectedCollectionsManager.java
+++ src/main/java/com/marklogic/mgmt/resource/security/ProtectedCollectionsManager.java
@@ -3,6 +3,9 @@ package com.marklogic.mgmt.resource.security;
 import com.marklogic.mgmt.resource.AbstractResourceManager;
 import com.marklogic.mgmt.ManageClient;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class ProtectedCollectionsManager extends AbstractResourceManager {
 
     public ProtectedCollectionsManager(ManageClient client) {
@@ -26,12 +29,28 @@ public class ProtectedCollectionsManager extends AbstractResourceManager {
 
     @Override
     public String getPropertiesPath(String resourceNameOrId, String... resourceUrlParams) {
-        return getResourcesPath() + "/properties?collection=" + resourceNameOrId;
+        return getResourcesPath() + "/properties?collection=" + encodeCollectionName(resourceNameOrId);
     }
 
     @Override
     public String getResourcePath(String resourceNameOrId, String... resourceUrlParams) {
-        return getResourcesPath() + "?collection=" + resourceNameOrId;
+        return getResourcesPath() + "?collection=" + encodeCollectionName(resourceNameOrId);
     }
 
+	/**
+	 * For most Manage API resources, MarkLogic prohibits characters that require URL encoding. Collection names are
+	 * an exception though and thus require URL encoding so that their names can be used in a querystring.
+	 *
+	 * @param collectionName
+	 * @return
+	 */
+	private String encodeCollectionName(String collectionName) {
+		try {
+			return URLEncoder.encode(collectionName, "utf-8");
+		} catch (UnsupportedEncodingException e) {
+			logger.warn(format("Unable to encode collection: %s; will include un-encoded collection name in " +
+				"querystring; cause: %s", collectionName, e.getMessage()));
+			return collectionName;
+		}
+	}
 }

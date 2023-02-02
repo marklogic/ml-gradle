9bef9f809b415187cf4d7f3ca0fa72e60fa623f5
Rob Rudin <rob.rudin@marklogic.com>
2023-02-02 15:32:23 -0500

    Removed OBE travis file

--- .travis/install-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] ; then
-  ./.travis/travis-install-ml.sh release
-  ./.travis/setup-marklogic.sh
-fi
--- .travis/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Can't run "test" because it exceeds the time limit for a Travis job
-# Also get intermittent 503 errors when running via Travis
-
-if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] ; then
-  ./gradlew clean compiletestjava
-fi

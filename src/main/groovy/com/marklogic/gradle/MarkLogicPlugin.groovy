2a8c72be1b72c88eef74fbfd6021481adfaad1dd
Rob Rudin <rob.rudin@progress.com>
2023-09-01 16:27:30 -0400

    DEVEXP-547 Can now select suites and tests to run
    Updated the unit-test project temporarily with snapshot dependencies for easy local testing before the release. 
    
    Cleaned up UnitTestTask a bit by extracting a couple methods.

--- src/main/groovy/com/marklogic/gradle/MarkLogicPlugin.groovy
+++ src/main/groovy/com/marklogic/gradle/MarkLogicPlugin.groovy
@@ -350,6 +350,8 @@ class MarkLogicPlugin implements Plugin<Project> {
 				"Can use -PsuiteName to override the name of the test suite, -PtestName to override the name of the test module, and -Planguage to specify \"sjs\" or \"xqy\" test code.")
 		project.task("mlUnitTest", type: UnitTestTask, group: unitTestGroup, description: "Run tests found under /test/suites in the modules database. " +
 			"Connects to MarkLogic via the REST API server defined by mlTestRestPort (or by mlRestPort if mlTestRestPort is not set), and uses mlRest* properties for authentication. " +
+			"Use -Psuites to specify one or more suite names, separated by commas, to run. If not set, all suites will be run. " +
+			"Use -Ptests to specify one or more test names, separated by commas, to run. If not set, all tests in a suite will be run. " +
 			"Use -PunitTestResultsPath to override where test result files are written, which defaults to build/test-results/marklogic-unit-test. " +
 			"Use -PrunCodeCoverage to enable code coverage support when running the tests. " +
 			"Use -PrunTeardown and -PrunSuiteTeardown to control whether teardown and suite teardown scripts are run; these default to 'true' and can be set to 'false' instead. ")

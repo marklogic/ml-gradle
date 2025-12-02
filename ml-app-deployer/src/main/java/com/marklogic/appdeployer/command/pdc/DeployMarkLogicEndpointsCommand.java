dbc426b4f2dabf08d6574a8f5aa6fb8370c3ef58
Rob Rudin <rudin@progress.com>
2025-12-02 11:27:22 -0500

    MLE-25605 Only deploying endpoints that are new or updated
    Still relying on manual testing for this.

--- ml-app-deployer/src/main/java/com/marklogic/appdeployer/command/pdc/DeployMarkLogicEndpointsCommand.java
+++ ml-app-deployer/src/main/java/com/marklogic/appdeployer/command/pdc/DeployMarkLogicEndpointsCommand.java
@@ -11,6 +11,7 @@ import com.progress.pdc.client.PdcClient;
 import com.progress.pdc.client.generated.ApiException;
 import com.progress.pdc.client.generated.api.ServiceApi;
 import com.progress.pdc.client.generated.model.MarkLogicApp;
+import com.progress.pdc.client.generated.model.MarkLogicEndpointMappingData;
 import com.progress.pdc.client.generated.model.MarkLogicHttpEndpoint;
 import org.springframework.util.StringUtils;
 
@@ -18,9 +19,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DeployMarkLogicEndpointsCommand extends AbstractCommand {
@@ -43,9 +44,6 @@ public class DeployMarkLogicEndpointsCommand extends AbstractCommand {
 			if (!StringUtils.hasText(context.getAppConfig().getCloudApiKey())) {
 				logger.warn("Found configuration for {} MarkLogic endpoint(s), but not deploying them because no cloud API key has been specified.", endpoints.size());
 			} else {
-				if (logger.isInfoEnabled()) {
-					logger.info("Deploying {} MarkLogic endpoint(s)", endpoints.size());
-				}
 				deployEndpoints(context, endpoints);
 			}
 		}
@@ -100,12 +98,73 @@ public class DeployMarkLogicEndpointsCommand extends AbstractCommand {
 		final String host = context.getManageClient().getManageConfig().getHost();
 		try (PdcClient pdcClient = new PdcClient(host, context.getAppConfig().getCloudApiKey())) {
 			final UUID markLogicServiceId = getFirstMarkLogicServiceId(pdcClient);
+			final ServiceApi serviceApi = new ServiceApi(pdcClient.getApiClient());
 			try {
-				new ServiceApi(pdcClient.getApiClient()).apiServiceMlendpointsIdHttpPut(markLogicServiceId, endpoints);
+				MarkLogicEndpointMappingData existingEndpoints = serviceApi.apiServiceMlendpointsIdGet(markLogicServiceId);
+				List<MarkLogicHttpEndpoint> endpointsToDeploy = filterOutExistingEndpoints(endpoints, existingEndpoints);
+				if (endpointsToDeploy.isEmpty()) {
+					logger.info("All {} endpoint(s) are up to date; nothing to deploy.", endpoints.size());
+				} else {
+					if (logger.isInfoEnabled()) {
+						logger.info("Deploying {} new or updated endpoint(s) out of {} total.", endpointsToDeploy.size(), endpoints.size());
+					}
+					serviceApi.apiServiceMlendpointsIdHttpPut(markLogicServiceId, endpointsToDeploy);
+					logger.info("Successfully deployed {} endpoint(s).", endpointsToDeploy.size());
+				}
 			} catch (ApiException e) {
 				throw new RuntimeException("Unable to create MarkLogic endpoints in PDC; cause: %s".formatted(e.getMessage()), e);
 			}
 		}
+	}
+
+	/**
+	 * Filters out endpoints that don't need to be deployed. An endpoint needs to be deployed if:
+	 * 1. It doesn't exist in PDC (based on name, which is unique), OR
+	 * 2. It exists but has different properties (needs to be updated)
+	 * <p>
+	 * An endpoint can take a surprisingly long time to create, so we only want to deploy ones that
+	 * are new or have changed.
+	 *
+	 * @param endpoints            the list of endpoints to potentially deploy
+	 * @param existingEndpointData the existing endpoint data from PDC
+	 * @return a list of endpoints that need to be deployed (new or updated)
+	 */
+	private List<MarkLogicHttpEndpoint> filterOutExistingEndpoints(
+		List<MarkLogicHttpEndpoint> endpoints,
+		MarkLogicEndpointMappingData existingEndpointData
+	) {
+		if (existingEndpointData == null || existingEndpointData.getEndpoints() == null
+			|| existingEndpointData.getEndpoints().getHttpEndpoints() == null) {
+			return endpoints;
+		}
+
+		Map<String, MarkLogicHttpEndpoint> existingEndpointsByName = existingEndpointData.getEndpoints()
+			.getHttpEndpoints().stream()
+			.collect(Collectors.toMap(MarkLogicHttpEndpoint::getName, Function.identity()));
+
+		return endpoints.stream()
+			.filter(endpoint -> needsDeployment(endpoint, existingEndpointsByName.get(endpoint.getName())))
+			.collect(Collectors.toList());
+	}
+
+	/**
+	 * Determines if an endpoint needs to be deployed by comparing it to an existing endpoint.
+	 *
+	 * @param endpoint         the endpoint to potentially deploy
+	 * @param existingEndpoint the existing endpoint with the same name, or null if none exists
+	 * @return true if the endpoint needs to be deployed (new or has changes)
+	 */
+	private boolean needsDeployment(MarkLogicHttpEndpoint endpoint, MarkLogicHttpEndpoint existingEndpoint) {
+		if (existingEndpoint == null) {
+			return true;
+		}
+
+		// We don't check "type", as this command so far only supports http endpoints.
+		return !Objects.equals(endpoint.getDisplayName(), existingEndpoint.getDisplayName())
+			|| !Objects.equals(endpoint.getIcon(), existingEndpoint.getIcon())
+			|| !Objects.equals(endpoint.getPath(), existingEndpoint.getPath())
+			|| !Objects.equals(endpoint.getPort(), existingEndpoint.getPort())
+			|| !Objects.equals(endpoint.getSupportedByCloud(), existingEndpoint.getSupportedByCloud());
 	}
 
 	private UUID getFirstMarkLogicServiceId(PdcClient pdcClient) {
